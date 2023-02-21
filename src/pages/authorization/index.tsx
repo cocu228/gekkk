ab622e82bc39e4925598d408058c1c431389649f
muzhichina <aleksey.muzhichin@gmail.com>
2023-02-21 15:42:22 +0300

    page authorization

--- src/pages/authorization/index.tsx
+++ src/pages/authorization/index.tsx
@@ -1,15 +1,16 @@
-import styles from './style.module.scss';
 import {Input} from 'antd';
 import Form from "../../entities/form/Form";
 import FormItem from "../../entities/form/form-item/FormItem";
-import {useState} from "react";
-import InputPassword from "../../entities/input-password";
+import React, {useCallback, useState} from "react";
 import {AnyMaskedOptions} from 'imask';
 import useMask from '../../features/useMask';
 import useValidation from '../../features/useValidation';
+import Button from "../../entities/button/Button";
+import {passwordMessage, phoneMessage} from "../../processes/message";
+import {RuleRender} from "antd/es/form";
 
 export const MASK_PHONE: AnyMaskedOptions = {
-    mask: '+7 (000) 000-00-00',
+    mask: '+{7} (000) 000-00-00',
 };
 
 function Authorization() {
@@ -17,35 +18,63 @@ function Authorization() {
     const {phoneValidator} = useValidation();
     const [toggle, setToggle] = useState(true)
 
+    const validationPassword = useCallback<RuleRender>(
+        () => ({
+            validator(rule, value = '') {
+
+                return new Promise((resolve, reject) => {
+                    if (value) {
+                        resolve('');
+                    } else {
+                        reject();
+                    }
+                });
+            },
+        }),
+        [],
+    );
+    const onSubmit = (event: unknown) => {
+        console.log(event)
+    }
 
     return (
-        <div className={`w-25 px-4 ${styles.App}`}>
-            <div className="wrapper">
-                <div className="grid grid-rows-1 justify-center pb-6">
-                    <img width={72} height={24} src="public/logo.png" alt="logo"/>
+        <div className="w-full h-full relative">
+            <div className="wrapper absolute top-0 left-0 right-0 px-4 pt-6">
+                <div className="wrapper">
+                    <div className="grid grid-rows-1 justify-center pb-6">
+                        <img width={72} height={24} src="public/logo.png" alt="logo"/>
+                    </div>
                 </div>
-            </div>
-            <div className="grid justify-center pb-10">
-                <div className="gap-2 inline-grid grid-cols-2 grid-rows-1">
+                <div className="grid justify-center pb-10">
+                    <div className="gap-2 inline-grid grid-cols-2 grid-rows-1">
                     <span onClick={() => setToggle(prev => !prev)}
-                          className={`${toggle ? "active border-b-2 text-center border-b-blue-600" : ""}`}>Login</span>
-                    <span onClick={() => setToggle(prev => !prev)}
-                          className={`${!toggle ? "active text-center border-b-2 border-b-blue-600" : "text-center"}`}>Create</span>
+                          className={`${toggle ? "active border-b-2 text-center border-b-blue-600" : "text-center"}`}>Login</span>
+                        <span onClick={() => setToggle(prev => !prev)}
+                              className={`${!toggle ? "active text-center border-b-2 border-b-blue-600" : "text-center"}`}>Create</span>
+                    </div>
                 </div>
+                {toggle && <Form onFinishFailed={onSubmit} onFinish={onSubmit}>
+                    <FormItem name="phone" label="Телефон" preserve
+                              rules={[{required: true, ...phoneMessage}, phoneValidator]}>
+                        <Input
+                            type="tel"
+                            placeholder="Phone number"
+                            onInput={onInput}
+                            autoComplete="tel"
+                        />
+                    </FormItem>
+                    <FormItem name="password" label="Password"
+                              rules={[{required: true, ...passwordMessage}, validationPassword]}>
+                        <Input.Password placeholder="Password"/>
+                    </FormItem>
+                    <div className="row text-right mb-9">
+                        <a className="text-sm text-blue-700 font-bold" href="#">Forgot password?</a>
+                    </div>
+                    <div className="row">
+                        <Button htmlType="submit" className={"w-full"}>Login</Button>
+                    </div>
+                </Form>}
             </div>
-            {toggle && <Form>
-                <FormItem name="phone" label="Телефон" preserve>
-                    <Input
-                        type="tel"
-                        placeholder="Телефон"
-                        onInput={onInput}
-                        autoComplete="tel"
-                    />
-                </FormItem>
-                <FormItem>
-                    <InputPassword/>
-                </FormItem>
-            </Form>}
         </div>
     )
 }

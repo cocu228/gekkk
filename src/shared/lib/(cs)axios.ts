36cd01c0b17a68fe6b2c3cb1efbf7c13a90031cf
muzhichina <aleksey.muzhichin@gmail.com>
2023-03-01 16:51:58 +0300

    +change baseURL:https://api-dev.gekkoin.com/api/v1

--- src/shared/lib/(cs)axios.ts
+++ src/shared/lib/(cs)axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const $axios = axios.create({
     withCredentials: true,
-    baseURL: "http://api-dev.gekkoin.com/api/v1"
+    baseURL: "https://api-dev.gekkoin.com/api/v1"
 });
 
 export default $axios;

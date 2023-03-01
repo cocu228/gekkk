9272a5c7d66310b4d6ba6067a5e0478fe945fd0a
muzhichina <aleksey.muzhichin@gmail.com>
2023-03-01 15:53:29 +0300

    +update package.json +change baseURL:http://api-dev.gekkoin.com/api/v1

--- src/shared/lib/(cs)axios.ts
+++ src/shared/lib/(cs)axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const $axios = axios.create({
     withCredentials: true,
-    baseURL: "https://bgate-dev.star-bridge.lv/api/v1"
+    baseURL: "http://api-dev.gekkoin.com/api/v1"
 });
 
 export default $axios;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig, loadEnv} from 'vite';
 // import tailwindcss from 'tailwindcss'
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
 import path from "path";
 
 const root = path.resolve(__dirname, 'src');

ae0aaa22a7031016138fa2b12a7693d21dd7e3c4
rezucos <tsyganok.vasya@yandex.ru>
2023-03-03 14:02:59 +0300

    Отображение шрифтов в auth

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.scss'
+import { ConfigProvider } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App/>
+        <ConfigProvider theme = {{
+            token: {
+                fontFamily: 'inherit'
+            },
+        }}>
+            <App/>
+        </ConfigProvider>
     </React.StrictMode>
 )

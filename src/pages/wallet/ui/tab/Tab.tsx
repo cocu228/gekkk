32d35b794325e641758a2aa6874767c19481543d
ReZuCoS <tsyganok.vasya@yandex.ru>
2023-03-12 23:36:17 +0300

    Добавлен стиль и логика для Tab

---
+++ src/pages/wallet/ui/tab/Tab.tsx
@@ -0,0 +1,25 @@
+import {MouseEventHandler} from 'react';
+import styles from './style.module.scss';
+
+type TabParams = {
+    children: JSX.Element | null,
+    isActive?: boolean,
+    onClick?: MouseEventHandler,
+}
+
+const Tab = ({
+    children,
+    isActive = false,
+    onClick = undefined,
+}: TabParams) => {
+    return (
+        <button
+            className={`${styles.Tab} ${isActive && styles.ActiveTab}`}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Tab;

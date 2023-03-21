50a3d1f9889148f21b544aeef5d1d5a5d37c05e9
rezucos <tsyganok.vasya@yandex.ru>
2023-03-21 14:24:57 +0300

    Added api request

---
+++ src/shared/api/gekkoin/tokens-networks.ts
@@ -0,0 +1,29 @@
+import $axios from "@/shared/lib/(cs)axios";
+
+interface IResTokenNetworks {
+    id: number,
+    type_network: number,
+    type_network_name?: string,
+    address_contract?: string,
+    type_contract?: string,
+    withdraw_fee: number,
+    name?: string,
+    symbol?: string,
+    is_operable: boolean,
+    min_withdraw: number,
+    min_deposit: number,
+    max_withdraw: number,
+    can_withdraw: boolean,
+    can_deposit: boolean,
+    type_address?: string
+}
+
+export const apiTokenNetworks = (currency: string) =>
+    $axios.get<IResTokenNetworks[]>('/gek/v1/tokens_networks', {
+        params: {
+            currency
+        },
+        transformResponse: [(data) => {
+            return JSON.parse(data)
+        }],
+    })

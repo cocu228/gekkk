51cc4972054cef3742645d102dfd5b22b2d947a2
rezucos <tsyganok.vasya@yandex.ru>
2023-03-21 10:27:28 +0300

    History: filters changed to secondaryTabs

--- src/widgets/dashboard/ui/History.tsx
+++ src/widgets/dashboard/ui/History.tsx
@@ -1,18 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import SectionTitle from "@/shared/ui/section-title/SectionTitle";
-import Filter from "@/shared/ui/filter/Filter";
+import SecondaryTabGroup from "@/shared/ui/tab-group/secondary";
 import Table from "@/shared/ui/table/Table";
 import {apiHistoryTransactions} from "@/shared/api";
 import {sameOrAfter} from "@/shared/lib/date-helper";
 import {format, isSameSecond, isAfter, isBefore, subDays, subYears} from "date-fns";
 
-const historyFilters = [
-    {value: 'month', label: 'This month'},
-    {value: '30_days', label: 'Last 30 days'},
-    {value: '90_days', label: 'Last 90 days'},
-    {value: 'year', label: 'This year'},
-    {value: 'custom', label: 'Custom period'},
-];
+const historyTabs: Record<string, string> = {
+    'month': 'This month',
+    '30_days': 'Last 30 days',
+    '90_days': 'Last 90 days',
+    'year': 'This year',
+    'custom': 'Custom period',
+};
 const filter = (historyList, selectedOption) => {
 
     switch (selectedOption) {
@@ -35,7 +35,7 @@ const filter = (historyList, selectedOption) => {
 
 function History() {
 
-    const [selectedOption, setSelectedOption] = useState(historyFilters[0].value);
+    const [activeTab, setActiveTab] = useState(Object.keys(historyTabs)[0]);
     const [historyList, setHistoryList] = useState([]);
 
 
@@ -47,7 +47,7 @@ function History() {
         })()
     }, [])
 
-    let filterList = filter(historyList, selectedOption)
+    let filterList = filter(historyList, activeTab)
 
     const sortedList = filterList.map((item) => ([{text: format(new Date(item.datetime), "dd MMMM yyyy HH:mm")}, {
         text: <span className="text-green">{item.amount + " " + item.currency}</span>
@@ -57,7 +57,7 @@ function History() {
         <div className="wrapper">
             <SectionTitle>History</SectionTitle>
             <div className="bg-white rounded-[6px] p-[15px] shadow-[0_4px_12px_0px_rgba(0,0,0,0.12)]">
-                <Filter options={historyFilters} selected={selectedOption} onChange={setSelectedOption}/>
+                <SecondaryTabGroup tabs={historyTabs} activeTab={activeTab} setActiveTab={setActiveTab}/>
                 <Table
                     data={{
                         labels: [{text: 'Data'}, {text: 'Flow of funds'}, {text: 'Information'}],

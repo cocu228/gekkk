fa60100ac089bd160b17e497ef46e4c69461b0ef
rezucos <tsyganok.vasya@yandex.ru>
2023-03-22 13:29:24 +0300

    Added address textbox with copy button

--- src/widgets/wallet-stage/top-up/ui/crypto/CryptoTopUp.tsx
+++ src/widgets/wallet-stage/top-up/ui/crypto/CryptoTopUp.tsx
@@ -1,17 +1,27 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 import Button from "@/shared/ui/button/Button";
 import Select from "@/shared/ui/select/Select";
 import ReactQRCode from 'react-qr-code';
-import { Input } from 'antd';
+import { Input, InputRef } from 'antd';
 
 const CryptoTopUp = () => {
     const [activeNetwork, setActiveNetwork] = useState('Test1');
+    const inputRef = useRef<InputRef>(null);
+    const walletAddress = '7Basgq7cazmjDXb43jQPrS82sZFWGXiyCBm4miA5MacNdX1WB4SNpfj7mUdoKFTpVPLBF5zjFNPUgZcDRTvsqpySCHtUeeM';
 
     let tokenNetworks: {label: string, value: string}[] = [
         {label: 'Test1', value: 'Test1'},
         {label: 'Test2', value: 'Test2'},
         {label: 'Test3', value: 'Test3'}
     ];
+
+    const copyAddress = () => {
+        inputRef.current.focus({
+            cursor: 'all',
+        });
+
+        navigator.clipboard.writeText(walletAddress)
+    }
 
     return (
         <div className="flex flex-col items-center mt-2">
@@ -46,17 +56,28 @@ const CryptoTopUp = () => {
                             <ReactQRCode
                                 size={148}
                                 style={{height: "auto", maxWidth: "100%", width: "100%"}}
-                                value={'hash'}
+                                value={walletAddress}
                                 viewBox={`0 0 148 148`}
                             />
                         </div>
                     </div>
 
-                    <Input
-                        className='h-[20px]'
-                        readOnly={true}
-                        value={'WALLET_ADDRESS'}
-                    />
+                    <div className='flex grow shrink container h-full'>
+                        <Input
+                            ref={inputRef}
+                            className='flex !border-[#d9d9d9] !shadow-none'
+                            readOnly={true}
+                            value={walletAddress}
+                            suffix={
+                                <img
+                                    onClick={copyAddress}
+                                    className='opacity-50 mx-2 hover:cursor-pointer hover:opacity-100'
+                                    src={`/img/icon/Copy.svg`}
+                                    alt='copy'
+                                />
+                            }
+                        />
+                    </div>
                 </div>
             )}
         </div>

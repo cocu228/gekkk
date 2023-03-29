010acb90d673d6def804c3e532e6f0a19ad0cfd5
muzhichina <aleksey.muzhichin@gmail.com>
2023-03-29 10:14:56 +0300

    ++ edits sidebar

--- src/widgets/footer/index.tsx
+++ src/widgets/footer/index.tsx
@@ -4,11 +4,13 @@ const Footer = ({textAlight}: { textAlight: string }) => {
 
     return <>
         <footer className="bg-gray-50 py-4">
-            <div className="row flex justify-start mb-2 px-4">
-                <span className={`${textAlight} text-gray-500 font-semibold text-sm`}>© Gekkoin. v.1.3</span>
+            <div className="row mb-2 px-4">
+                <span
+                    className={`${textAlight} text-gray-500 font-semibold text-sm w-full block`}>© Gekkoin. v.1.3</span>
             </div>
             <div className="wrapper px-4">
-                <p className={`${textAlight} text-gray-400 font-medium leading-4 text-xs`}>Crypto exchange service is powered
+                <p className={`${textAlight} text-gray-400 font-medium leading-4 text-xs`}>Crypto exchange service is
+                    powered
                     by AtlantEX OU (licensed partner
                     for crypto wallet and exchange)</p>
             </div>

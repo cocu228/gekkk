49b6ecbe7091c740f4ca2cf96bff468397619408
rezucos <tsyganok.vasya@yandex.ru>
2023-03-30 17:43:11 +0300

    Отображение открытых ордеров клиента

--- src/widgets/exchange/ui/open-orders/OpenOrders.tsx
+++ src/widgets/exchange/ui/open-orders/OpenOrders.tsx
@@ -1,38 +1,34 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { format } from 'date-fns';
+import { IResOrder, apiGetOrders } from '@/shared/api';
+import { storeListAllCryptoName } from '@/shared/store/crypto-assets';
 import styles from './style.module.scss';
 
-const orders = [
-    {
-        id: 1,
-        payValue: '62,5',
-        payToken: 'EURG',
-        getValue: '0,5',
-        getToken: 'XMR',
-        date: '01/12/2022, 17:45',
-        price: '125,04',
-        info: '(sale at the current market rate)'
-    },
-    {
-        id: 2,
-        payValue: '62,5',
-        payToken: 'EURG',
-        getValue: '0,5',
-        getToken: 'XMR',
-        date: '01/12/2022, 17:45',
-        price: '125,04'
-    },
-    {
-        id: 3,
-        payValue: '62,5',
-        payToken: 'EURG',
-        getValue: '0,5',
-        getToken: 'XMR',
-        date: '01/12/2022, 17:45',
-        price: '125,04'
-    }
-];
+const getOrderPrice = (order: IResOrder) => {
+    const ratio = order.volume_dest / order.volume_source
+
+    return ratio
+}
 
 function OpenOrders() {
+    const assets = storeListAllCryptoName(state => state.listAllCryptoName);
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+
+        (async () => {
+
+            const response = await apiGetOrders();
+            setOrders(response.data);
+
+        })()
+
+    }, []);
+
+    // TODO: округление чисел
+    const currencyPrecision = (value: number, currency: string) =>
+        value.toFixed(assets.find(a => a.code === currency)?.round_prec);
+
     return (
         <>
             <div className="flex justify-between">
@@ -40,22 +36,29 @@ function OpenOrders() {
                 <a className="text-xs text-secondary font-medium" href="">All</a>
             </div>
             <div className="mt-1.5">
-                {orders.map(orderItem => (
-                    <div className={`py-2 text-xs rounded-md ${styles.Item}`} key={orderItem.id}>
+                {orders.map((order: IResOrder) => (
+                    <div className={`py-2 text-xs rounded-md ${styles.Item}`} key={order.id}>
                         <div className="flex justify-between">
                             <div className="text-orange bg-orange bg-opacity-10 rounded-md">
-                                <strong>{orderItem.payValue}</strong> ${orderItem.payToken} &rarr; <strong>{orderItem.getValue}</strong> {orderItem.getToken}
+                                <strong>
+                                    {/*currencyPrecision(*/order.volume_source/*, order.from)*/}
+                                </strong> {order.from} &rarr; <strong>
+                                    {order.type_order === 'Limit' ? '' : '~'}
+                                    {/*currencyPrecision(*/order.volume_dest/*, order.to)*/}
+                                </strong> {order.to}
                             </div>
-                            <div className="text-secondary">{orderItem.date}</div>
+                            <div className="text-secondary">{
+                                format(new Date(order.time_created), 'dd/MM/yyyy HH:mm')
+                            }</div>
                         </div>
                         <div className="flex justify-between mt-1">
                             <div className="flex gap-2.5">
                                 <div className="text-secondary">Price: </div>
                                 <div>
-                                    <span>1 ${orderItem.payToken} ~ ${orderItem.price} ${orderItem.getToken}</span>&nbsp;
-                                    {orderItem.info && (
+                                    <span>1 {order.from} ~ {getOrderPrice(order)} {order.to}</span>&nbsp;
+                                    {/*orderItem.info && (
                                         <span>{orderItem.info}</span>
-                                    )}
+                                    )*/}
                                 </div>
                             </div>
                             <button className="text-secondary">Cancel</button>

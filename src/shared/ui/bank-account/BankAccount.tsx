7f0b63a2c0efd0c1e16919402499ba7b883ae943
rezucos <tsyganok.vasya@yandex.ru>
2023-04-05 12:27:40 +0300

    Added bank account card

---
+++ src/shared/ui/bank-account/BankAccount.tsx
@@ -0,0 +1,23 @@
+import Card from "@/shared/ui/card/Card";
+
+interface AccoundData {
+    iban: string;
+    balance: number;
+    currency: string;
+}
+
+const BankAccount = ({iban, balance, currency}: AccoundData) => {
+    const formattedIBAN = iban.slice(0, 9) + '***' + iban.slice(-4);
+
+    return (
+        <Card>
+            <div className='font-semibold text-lg mb-auto'>IBAN: {formattedIBAN}</div>
+            <div className='font-semibold text-gray-400'>Balance:</div>
+            <div className='font-medium uppercase text-sm'>
+                <span className="text-[32px] font-bold">{balance}</span> {currency}
+            </div>
+        </Card>
+    )
+}
+
+export default BankAccount;

61e55259318dcee5ae8916a25b45a6d7c53ab52f
rezucos <tsyganok.vasya@yandex.ru>
2023-04-05 11:53:15 +0300

    removed card class using

--- src/shared/ui/bank-card/BankCard.tsx
+++ src/shared/ui/bank-card/BankCard.tsx
@@ -1,4 +1,3 @@
-import Card from '@/shared/ui/card/Card';
 import Styles from './style.module.scss';
 
 interface CardData {
@@ -9,18 +8,29 @@ interface CardData {
 
 const BankCard = ({cardNumber, expiresAt, holderName}: CardData) => {
     return (
-        <div className='max-w-[310px]'>
-            <Card className='flex opacity-95 bg-[url(/img/payment-card/payment-card-background.png)] hover:bg-[url(/img/payment-card/payment-card-background.png)] hover:opacity-100 p-0'>
-                <img
-                    src='/img/payment-card/payment-card.svg'
-                    className='absolute'
-                />
-                <div className={`${Styles.CardData} ml-5 mt-[110px]`}>
-                    <div className='text-[22px] mb-2'>{cardNumber}</div>
-                    <div>{expiresAt}</div>
-                    <div>{holderName}</div>
-                </div>
-            </Card>
+        <div className='
+                flex
+                p-0
+                opacity-95
+                bg-[url(/img/payment-card/payment-card-background.png)]
+                hover:bg-[url(/img/payment-card/payment-card-background.png)]
+                hover:opacity-100
+                hover:shadow-[0_10px_27px_0px_rgba(0,0,0,0.16)]
+                min-h-[195px]
+                max-w-[310px]
+                sm:min-h-[216px]
+                min-w-[310px]
+                transition-all
+                rounded-[4px]'>
+            <img
+                src='/img/payment-card/payment-card.svg'
+                className='absolute'
+            />
+            <div className={`${Styles.CardData} ml-5 mt-[110px]`}>
+                <div className='text-[22px] mb-2'>{cardNumber}</div>
+                <div>{expiresAt}</div>
+                <div>{holderName}</div>
+            </div>
         </div>
     )
 }

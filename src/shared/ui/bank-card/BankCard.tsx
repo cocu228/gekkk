6c965ba9138502609d66ea9cc299423d944a1746
rezucos <tsyganok.vasya@yandex.ru>
2023-04-10 12:30:14 +0300

    Стиль банковской карты вынесен в модуль

--- src/shared/ui/bank-card/BankCard.tsx
+++ src/shared/ui/bank-card/BankCard.tsx
@@ -1,4 +1,4 @@
-import Styles from './style.module.scss';
+import styles from './style.module.scss';
 
 interface CardData {
     cardNumber: string;
@@ -8,25 +8,12 @@ interface CardData {
 
 const BankCard = ({cardNumber, expiresAt, holderName}: CardData) => {
     return (
-        <div className='
-                flex
-                p-0
-                opacity-95
-                bg-[url(/img/payment-card/payment-card-background.png)]
-                hover:bg-[url(/img/payment-card/payment-card-background.png)]
-                hover:opacity-100
-                hover:shadow-[0_10px_27px_0px_rgba(0,0,0,0.16)]
-                min-h-[195px]
-                max-w-[310px]
-                sm:min-h-[216px]
-                min-w-[310px]
-                transition-all
-                rounded-[4px]'>
+        <div className={`${styles.Card} sm:min-h-[216px]`}>
             <img
                 src='/img/payment-card/payment-card.svg'
                 className='absolute'
             />
-            <div className={`${Styles.CardData} ml-5 mt-auto`}>
+            <div className={`${styles.Data} ml-5 mt-auto`}>
                 <div className='text-[22px] mb-2'>{cardNumber}</div>
                 <div>{expiresAt}</div>
                 <div className='mb-3'>{holderName}</div>

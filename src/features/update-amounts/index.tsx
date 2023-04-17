46122ac00213936a3c488086f499b001d709a99e
muzhichina <aleksey.muzhichin@gmail.com>
2023-04-17 15:56:11 +0400

    update-amounts > add animation

--- src/features/update-amounts/index.tsx
+++ src/features/update-amounts/index.tsx
@@ -1,23 +1,30 @@
 import {storeListAllCryptoName, storeListAvailableBalance} from "@/shared/store/crypto-assets";
+import {useState} from "react";
+import {isActiveClass} from "@/shared/lib/helpers";
+import styles from "./style.module.scss"
 
 const UpdateAmounts = () => {
+
     const getListAllCryptoName = storeListAllCryptoName(state => state.getListAllCryptoName)
     const getDefaultListBalance = storeListAvailableBalance(state => state.getDefaultListBalance)
     const setSortedListBalance = storeListAvailableBalance(state => state.setSortedListBalance)
+
+    const [active, setActive] = useState(false)
     const onClick = async () => {
-
+        setActive(true)
         const listAllCryptoName = await getListAllCryptoName()
-
-        console.log(listAllCryptoName)
 
         await getDefaultListBalance();
 
         setSortedListBalance(listAllCryptoName);
 
+        setTimeout(() => setActive(false), 3000)
+
     }
 
     return <>
-        <span data-text={"Update"} className="cursor-pointer ellipsis" onClick={onClick}>
+        <span data-text={"Update"} className={`cursor-pointer ellipsis ${styles.UpdateBtn} ${isActiveClass(active)}`}
+              onClick={onClick}>
             <img width={20} height={20} src="/img/icon/DepositCurrentRateIcon.svg" alt="DepositCurrentRateIcon"/>
         </span>
     </>

e84544a736a698bc1bbc363349145ab67a551831
shpakovn <nikita.shpakov.is@gmail.com>
2023-04-20 10:55:59 +0300

    Кошелек История

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -8,26 +8,9 @@ import Transfer from "@/widgets/wallet-stage/transfer/Transfer";
 import {IResListAddresses} from "@/shared/api";
 import {storeListAvailableBalance, storeListAddresses} from "@/shared/store/crypto-assets";
 import TabsGroupPrimary from "@/shared/ui/tabs-group/primary";
-import {IconCoin} from "@/shared/ui/icons/icon-coin";
-
-const EurgTooltipText: string = `We pay you 3% per annum of EURG on your balance under following conditions:\n
-(i) your weighted average balance for the reporting period is equal to or higher than 300 EURG\n
-(ii) our upper limit for the balance to pay the interest rate is 100,000 EURG.`;
-
-const EurgDescriptionText: string = `Utility token with a fixed rate\n1 EURG = 1 euro`;
-
-const actionsByFlag: Record<number, string> = {
-    2: 'Buy or Sell',
-    8: 'Top up, Exchange and Send',
-}
-
-function getDescriptionText(name: string, currency: string, flags: number) {
-    let actions: string = actionsByFlag[flags];
-    if (!actions)
-        return 'Short description for this currency is not done yet.';
-
-    return `${actions} your ${name} (${currency}) directly from your Gekkoin account`;
-}
+import WalletHeader from "@/widgets/wallet-stage/header/ui";
+import {useContext} from "react";
+import {BreakpointsContext} from "@/app/providers/BreakpointsProvider";
 
 function Wallet() {
 
@@ -35,63 +18,16 @@ function Wallet() {
     const sortedListBalance = storeListAvailableBalance(state => state.sortedListBalance)
     const currency = sortedListBalance.find(item => item.const === params.currency)
     const listAddresses: IResListAddresses[] = storeListAddresses(state => state.listAddresses);
+    const {xl, md} = useContext(BreakpointsContext);
 
     if (!currency) return null
 
-    const isEURG: boolean = currency.const === 'EURG';
-
     return (
         <div className="flex flex-col h-full w-full">
-            <div className="container flex mx-auto px-4">
-                <div className='flex w-inherit py-6 items-center'>
-                    <div className="flex justify-start">
-                        <div className="mr-6">
-                            <IconCoin code={currency.const}/>
-                        </div>
-
-                        <div className="flex flex-col content-around">
-                            <div className="text-sm font-medium text-gray-400">
-                                Wallet balance
-                            </div>
-
-                            <div className="text-2xl font-bold text-gray-600 cursor-help">
-                                {currency.const}
-                            </div>
-                        </div>
-
-                        {isEURG && (
-                            <div className='flex flex-col content-around ml-[50px]'>
-                                <div className="text-sm font-medium text-semilight">
-                                    Rate
-
-                                    <Tooltip text={EurgTooltipText}>
-                                        <div className="inline-block relative align-middle w-[14px] ml-1 cursor-help">
-                                            <img src="/img/icon/HelpIcon.svg" alt="tooltip"/>
-                                        </div>
-                                    </Tooltip>
-                                </div>
-
-                                <div className='text-gray-600 text-2xl'>
-                                    3% per annum
-                                </div>
-                            </div>
-                        )}
-                    </div>
-
-                    <div className="ml-auto text-right">
-                        <div className="font-bold text-fs32 leading-[48px] text-gray-600 mb-4">
-                            {currency.name} wallet
-                        </div>
-                        <div className="max-w-[450px] font-medium text-sm text-gray-400 whitespace-pre-line">
-                            {isEURG ? EurgDescriptionText : getDescriptionText(currency.name, currency.const, currency.defaultInfoToken.flags)}
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <WalletHeader currency={currency}/>
             <TabsGroupPrimary defaultInit={"TopUp"}>
-                <div className='grid grid-cols-2'>
-                    <div className="substrate">
+                <div className={`grid grid-cols-${xl ? "1" : "2"}`}>
+                    <div className="substrate z-10">
                         <TopUp
                             data-tab={"TopUp"}
                             listAddresses={listAddresses}
@@ -114,11 +50,13 @@ function Wallet() {
                             flags={currency.defaultInfoToken.flags}
                             currency={currency.const}
                         />
-                    </div>
 
-                    <History className={`substrate -ml-4 h-full`}
-                             currency={currency.const}
-                    />
+                        {xl && <History data-tab={"History"} currency={currency.const}/>}
+
+                    </div>
+                    {!xl && <div className="substrate z-0 -ml-4 h-full">
+                        <History currency={currency.const}/>
+                    </div>}
                 </div>
             </TabsGroupPrimary>
         </div>
--- src/widgets/history/ui/History.tsx
+++ src/widgets/history/ui/History.tsx
@@ -1,24 +1,26 @@
 import dayjs from 'dayjs';
 import {useEffect, useState} from 'react';
-import SectionTitle from "@/shared/ui/section-title/SectionTitle";
 import SecondaryTabGroup from "@/shared/ui/tabs-group/secondary";
-import Table from "@/shared/ui/table/Table";
 import Button from '@/shared/ui/button/Button';
 import {DatePicker} from 'antd';
 import {apiHistoryTransactions} from "@/shared/api";
 import {Props, TabKey} from "../model/types";
 import {historyTabs, getTabsAsRecord} from "../model/helpers";
-import {formatForDisplay, formatForCustomer} from "@/shared/lib/date-helper";
+import {formatForCustomer, formatForDisplay} from "@/shared/lib/date-helper";
 import {startOfMonth} from "date-fns";
 import Loader from '@/shared/ui/loader';
+import styles from "./style.module.scss"
 
 const {RangePicker} = DatePicker;
 
-function History({title, currency, className}: Partial<Props>) {
+function History({currency}: Partial<Props>) {
 
     const [activeTab, setActiveTab] = useState<string>(historyTabs[0].Key);
+
     const [historyList, setHistoryList] = useState([]);
+
     const [loading, setLoading] = useState(false);
+
     const [customDate, setCustomDate] = useState<[dayjs.Dayjs, dayjs.Dayjs]>(
         [dayjs(startOfMonth(new Date())), dayjs()]
     )
@@ -54,61 +56,65 @@ function History({title, currency, className}: Partial<Props>) {
 
     return (
         <div className="wrapper">
-            {title && (
-                <SectionTitle>{title}</SectionTitle>
-            )}
-            <div className={`${className} bg-white rounded-[10px] p-[15px] shadow-[0_4px_12px_0px_rgba(0,0,0,0.12)]`}>
-                <SecondaryTabGroup tabs={getTabsAsRecord(historyTabs)} activeTab={activeTab} setActiveTab={setActiveTab}/>
+            <SecondaryTabGroup tabs={getTabsAsRecord(historyTabs)} activeTab={activeTab} setActiveTab={setActiveTab}/>
+            {activeTab === TabKey.CUSTOM && (
+                <div className='flex flex-col mt-3 mb-5'>
+                    Enter period or choose from calendar
 
-                {activeTab === TabKey.CUSTOM && (
-                    <div className='flex flex-col mt-3 mb-5'>
-                        Enter period or choose from calendar
+                    <div className='flex grow-0 max-w-[400px]'>
+                        <RangePicker
+                            className='mt-2 w-full'
+                            value={customDate}
+                            onChange={setCustomDate}
+                        />
 
-                        <div className='flex grow-0 max-w-[400px]'>
-                            <RangePicker
-                                className='mt-2 w-full'
-                                value={customDate}
-                                onChange={setCustomDate}
-                            />
-
-                            <Button
-                                className='ml-5'
-                                disabled={loading}
-                                onClick={() => requestHistory()}
-                            >Apply</Button>
-                        </div>
+                        <Button
+                            className='ml-5'
+                            disabled={loading}
+                            onClick={() => requestHistory()}
+                        >Apply</Button>
                     </div>
-                )}
-
-                {loading && (
-                    <Loader className='relative mt-10'/>
-                )}
-
-                {!loading && (
-                    <Table
-                        data={{
-                            labels: [
-                                {text: 'Data'},
-                                {text: 'Flow of funds'},
-                                {text: 'Information'}
-                            ],
-                            rows: historyList.map((item) =>
-                                [
-                                    {text: formatForCustomer(item.datetime)},
-                                    {
-                                        text: <span className="text-green">
-                                        {item.amount + " " + item.currency}
-                                    </span>
-                                    },
-                                    {text: ""}
-                                ])
-                        }}
-                        noDataText="You don't have any transactions for this time."
-                    />
-                )}
+                </div>
+            )}
+            <div className={`${styles.Table}`}>
+                <div className={styles.TableHead}>
+                    <div data-text={"Data"} className="col col-span-5 ellipsis">
+                        <span>Data</span>
+                    </div>
+                    <div data-text={"Flow of funds"} className="col col-span-3 ellipsis">
+                        <span>Flow of funds</span>
+                    </div>
+                    <div data-text={"Information"} className="col col-span-4 ellipsis">
+                        <span>Information</span>
+                    </div>
+                </div>
+                <div className={styles.TableBody}>
+                    {loading ?
+                        <Loader/> : historyList.length > 0 ? historyList.map((item, i) =>
+                                <TableRow {...item}/>) :
+                            <div className={styles.Row}>
+                                <span>You don't have any transaction for this time.</span>
+                            </div>}
+                </div>
             </div>
         </div>
     );
+}
+
+const TableRow = ({amount, datetime, type_transaction}) => {
+    const dataCustomer = formatForCustomer(datetime)
+
+    return <div className={`row grid grid-cols-12 ${styles.Row} gap-4`}>
+        <div data-text={dataCustomer} className="col col-span-5 ellipsis">
+            <span>{dataCustomer}</span>
+        </div>
+        <div data-text={amount} className="col col-span-3 ellipsis">
+            <span className="text-green">{amount}</span>
+        </div>
+        <div data-text={type_transaction} className="col col-span-4 ellipsis">
+            <span>{type_transaction}</span>
+        </div>
+    </div>
 }
 
 export default History;

605d12834e7209f8062d7e945872ff7044df733e
ReZuCoS <tsyganok.vasya@yandex.ru>
2023-04-23 12:35:46 +0300

    Centered option text

--- src/shared/ui/select/Select.tsx
+++ src/shared/ui/select/Select.tsx
@@ -1,23 +1,40 @@
+import styles from './style.module.scss';
+import { DefaultOptionType } from 'antd/es/select';
 import {ComponentType, memo, SVGProps} from 'react';
 import {Select as SelectAntd, SelectProps} from 'antd';
-import styles from './style.module.scss';
 import IconDropdownArrow from '../icons/IconDropdownArrow';
+const {Option} = SelectAntd;
 
 interface Props extends SelectProps {
     prefixIcon?: ComponentType<SVGProps<SVGSVGElement>>;
+    options?: Array<DefaultOptionType>;
 }
 
-const Select = memo<Props>(({prefixIcon: Icon, ...props}): JSX.Element | null => {
+const Select = memo<Props>(({prefixIcon: Icon, options, ...props}): JSX.Element | null => {
     return (
         <div className={styles.Select}>
             <SelectAntd
                 {...props}
+                dropdownStyle={{
+                    padding: 0,
+                    border: '1px solid var(--color-gray-400)',
+                    textAlign: 'center'
+                }}
                 suffixIcon={
                     <div className={styles.DropdownIcon}>
                         <IconDropdownArrow/>
                     </div>
                 }
-            />
+            >
+                {options.map((option) => (
+                    <Option
+                        key={option.value}
+                        value={option.value}
+                    >
+                            {option.label}
+                    </Option>
+                ))}
+            </SelectAntd>
         </div>
     );
 });

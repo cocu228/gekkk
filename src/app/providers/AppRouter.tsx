5c7ae2a52b8bd995e5c5e035492a486ca6da9cb5
ReZuCoS <tsyganok.vasya@yandex.ru>
2023-04-25 09:54:00 +0300

    removed Exchange 'to' parameter

--- src/app/providers/AppRouter.tsx
+++ src/app/providers/AppRouter.tsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
                 ]
             },
             {
-                path: 'exchange/:to?',
+                path: 'exchange',
                 element: <Exchange/>
             },
             {

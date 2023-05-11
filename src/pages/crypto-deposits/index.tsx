181cc05bd6b7a49729c6e39e52dec2f62eea0b16
nikita <nikita.shpakov.ge@gmail.com>
2023-05-11 20:20:46 +0300

    crypto-deposits

--- src/pages/crypto-deposits/index.tsx
+++ src/pages/crypto-deposits/index.tsx
@@ -22,17 +22,17 @@ const CryptoDeposits = () => {
 
                     <div className="row mt-6">
                         <div className="col">
-                            <h4>0,8% per month</h4>
+                            <h4 className="font-bold">0,8% per month</h4>
                         </div>
                     </div>
                     <div className="row mt-6">
                         <div className="col">
-                            <p className="text-gray-400">The fixed rate deposit allows you to know exactly how much
+                            <p className="text-gray-400 font-semibold">The fixed rate deposit allows you to know exactly how much
                                 interest you'll earn on your
                                 savings over a specific period</p>
                         </div>
                     </div>
-                    <div  className="row flex flex-wrap mt-6 px-10 mb-3 gap-7">
+                    <div className="row flex flex-wrap mt-6 px-10 mb-3 gap-7">
                         <div className="col">
                             <div className="flex items-center gap-2 md:flex-col">
                                 <p className="text-gray-400 text-sm">Return</p>
@@ -54,9 +54,10 @@ const CryptoDeposits = () => {
                             </div>
                         </div>
                     </div>
-                    <div style={md ? {border: "1px solid #B4C0CD", borderRadius: "4px 4px 0px 0px"} : {}} className="wrapper bg-[var(--color-main-bg)] py-6 px-10 flex items-center">
+                    <div style={md ? {border: "1px solid #B4C0CD", borderRadius: "4px 4px 0px 0px"} : {}}
+                         className="wrapper bg-[var(--color-main-bg)] py-6 px-10 flex items-center">
                         <p className="font-bold mr-4">0,8% per month (9,6% annual)</p>
-                        <Button gray size="sm" className="!text-black !font-normal whitespace-nowrap !h-[auto]">Open
+                        <Button gray size="sm" className="!text-black !bg-[#DEE2E73D] !py-2 !text-sm !font-normal whitespace-nowrap !h-[auto]">Open
                             deposit</Button>
                     </div>
                 </section>
@@ -66,7 +67,7 @@ const CryptoDeposits = () => {
                             <h2 className="text-gray-600 text-fs32 font-bold">Structured deposits</h2>
                         </div>
                     </div>
-                    <div className="row grid gap-3 xxxl:grid-cols-3 xxl:grid-cols-2 xl:grid-cols-1">
+                    <div className="row grid gap-3 grid-cols-3 xxl:grid-cols-2 lg:grid-cols-1">
                         <div className="col h-full flex flex-col justify-between">
                             <div className="wrapper">
                                 <div className="row mt-6">
@@ -74,17 +75,18 @@ const CryptoDeposits = () => {
                                         <h4 className="font-bold text-lg mb-2">
                                             Safe strategy
                                         </h4>
-                                        <p className="text-gray-400">You profit even if the rate drops or grows
+                                        <p className="text-gray-400 font-semibold">You profit even if the rate drops or grows
                                             slowly</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="wrapper">
                                 <div className="row flex flex-wrap mt-6 px-10 mb-3 gap-7">
-                                    <R val={[true, false, false]}/>
-                                    <R val={[false, false, false]}/>
+                                    <R val={[true, null, null]}/>
+                                    <R val={[null, null, null]}/>
                                 </div>
-                                <div style={md ? {border: "1px solid #B4C0CD", borderRadius: "4px 4px 0px 0px"} : {}} className="row">
+                                <div style={md ? {border: "1px solid #B4C0CD", borderRadius: "4px 4px 0px 0px"} : {}}
+                                     className="row">
                                     <div className="col">
                                         <Row md={md} val={["16%", "4% annual"]} bgGray/>
                                         <Row md={md} val={["17%", "3% annual"]}/>
@@ -100,16 +102,17 @@ const CryptoDeposits = () => {
                                         <h4 className="font-bold text-lg mb-2">
                                             Balanced strategy
                                         </h4>
-                                        <p className="text-gray-400">Minimal risk</p>
+                                        <p className="text-gray-400 font-semibold">Minimal risk</p>
                                     </div>
                                 </div>
                             </div>
                             <div className="wrapper">
                                 <div className="row flex flex-wrap mt-6 px-10 mb-3 gap-7">
-                                    <R val={[true, false, false]}/>
-                                    <R val={[null, false, false]}/>
+                                    <R val={[true, true, null]}/>
+                                    <R val={[false, null, null]}/>
                                 </div>
-                                <div style={md ? {border: "1px solid #B4C0CD", borderRadius: "4px 4px 0px 0px"} : {}} className="row">
+                                <div style={md ? {border: "1px solid #B4C0CD", borderRadius: "4px 4px 0px 0px"} : {}}
+                                     className="row">
                                     <div className="col">
                                         <Row md={md} val={["20%", "0"]} bgGray/>
                                         <Row md={md} val={["23%", "up to 3%"]}/>
@@ -125,7 +128,7 @@ const CryptoDeposits = () => {
                                         <h4 className="font-bold text-lg mb-2">
                                             Dynamic strategy
                                         </h4>
-                                        <p className="text-gray-400">Good percentage when the rate is rising, limited
+                                        <p className="text-gray-400 font-semibold">Good percentage when the rate is rising, limited
                                             loss
                                             when the rate is falling</p>
                                     </div>
@@ -134,9 +137,10 @@ const CryptoDeposits = () => {
                             <div className="wrapper">
                                 <div className="row flex flex-wrap mt-6 px-10 mb-3 gap-7">
                                     <R val={[true, true, true]}/>
-                                    <R val={[null, null, false]}/>
+                                    <R val={[false, false, null]}/>
                                 </div>
-                                <div style={md ? {border: "1px solid #B4C0CD", borderRadius: "4px 4px 0px 0px"} : {}} className="row">
+                                <div style={md ? {border: "1px solid #B4C0CD", borderRadius: "4px 4px 0px 0px"} : {}}
+                                     className="row">
                                     <div className="col ">
                                         <Row md={md} val={["30%", "up to -10%"]} bgGray/>
                                         <Row md={md} val={["40%", "up to -20%"]}/>
@@ -160,17 +164,17 @@ const R = ({val}) => {
             <p className="text-gray-400 text-sm">Return</p>
             <div className="flex gap-1">
                 {val.map(it => <div
-                    className={`w-[0.5rem] h-[0.5rem]  ${it === null ? "bg-red-main" : it ? "bg-green" : "bg-gray-200"}   rounded-full`}/>)}
+                    className={`w-[0.5rem] h-[0.5rem]  ${it === null ? "bg-gray-200" : it ? "bg-green" : "bg-red-main"}   rounded-full`}/>)}
             </div>
         </div>
     </div>
 }
 
-const Row = ({bgGray = false, val, md=false}) => {
+const Row = ({bgGray = false, val, md = false}) => {
 
     return <div
         style={md ? {borderBottom: "1px solid #B4C0CD"} : {}}
-        className={`row ${!md && bgGray ? "bg-gray-main" : ""} py-6 px-6 grid grid-flow-col justify-start items-center gap-3`}>
+        className={`row ${!md && bgGray ? "bg-gray-main" : ""} py-4 px-6 grid grid-flow-col justify-start items-center gap-3`}>
         <div className="col row-auto justify-start flex h-full">
             <div className="row">
                 <div className="col">
@@ -185,7 +189,7 @@ const Row = ({bgGray = false, val, md=false}) => {
                 </div>
             </div>
             <div className="row">
-                <div  data-text={val[0]} className="col ellipsis">
+                <div data-text={val[0]} className="col ellipsis">
                     <p className="font-bold text-normal">{val[0]}</p>
                 </div>
             </div>
@@ -213,7 +217,7 @@ const Row = ({bgGray = false, val, md=false}) => {
         </div>
         <div className="col">
             <Button gray size="sm"
-                    className="!text-black !font-normal whitespace-nowrap !rounded-[20px] text-[14px] !h-[auto]">Open
+                    className="!text-black !py-2 !text-sm !font-normal whitespace-nowrap !bg-[#DEE2E73D] !rounded-[20px] text-[14px] !h-[auto]">Open
                 deposit</Button>
         </div>
     </div>

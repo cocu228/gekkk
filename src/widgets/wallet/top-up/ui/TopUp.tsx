99b5c545c0bbf36db8232907437a0850e856e841
muzhichina <aleksey.muzhichin@gmail.com>
2023-05-15 14:18:18 +0400

    Wallet

--- src/widgets/wallet/top-up/ui/TopUp.tsx
+++ src/widgets/wallet/top-up/ui/TopUp.tsx
@@ -3,13 +3,12 @@ import Loader from "@/shared/ui/loader";
 import ChoseNetwork from "@/widgets/wallet/top-up/ui/ChoseNetwork";
 import {CtxWalletCurrency, CtxWalletNetworks} from "@/widgets/wallet/model/context";
 import TopUpQR from "@/widgets/wallet/top-up/ui/TopUpQR";
-import TopUpEURG from "@/widgets/wallet/top-up/ui/EURG";
-import NetworkProvider from "@/widgets/wallet/model/NetworkProvider";
+import GekkardAccount from "@/widgets/wallet/top-up/ui/EURG/GekkardAccount";
 
 
 const TopUp = memo(() => {
 
-    const {loading = true} = useContext(CtxWalletNetworks)
+    const {loading = true, networkIdSelect} = useContext(CtxWalletNetworks)
     const currency = useContext(CtxWalletCurrency),
         isEURG = currency.const === "EURG"
 
@@ -17,7 +16,9 @@ const TopUp = memo(() => {
         {loading ? <Loader/> :
             <>
                 <ChoseNetwork/>
-                <TopUpQR/>
+                <>{networkIdSelect === 122 && isEURG ? <GekkardAccount/> :
+                    <TopUpQR/>
+                }</>
             </>}
     </div>)
 

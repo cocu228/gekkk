405e2889128997b819790c875348d3d9f8d33484
muzhichina <aleksey.muzhichin@gmail.com>
2023-05-17 15:40:22 +0400

    wallet

--- src/widgets/wallet/transfer/apply-code/index.tsx
+++ src/widgets/wallet/transfer/apply-code/index.tsx
@@ -6,11 +6,16 @@ import {apiApplyTxCode} from "@/shared/api";
 import {storeListTxCode} from "@/widgets/wallet/transfer/store/list-tx-code";
 import {actionResSuccess} from "@/shared/lib/helpers";
 import {storeListAllCryptoName, storeListAvailableBalance} from "@/shared/store/crypto-assets";
+import useModal from "@/shared/model/hooks/useModal";
+import Modal from "@/shared/ui/modal/Modal";
 
 const ApplyCode = () => {
     const [input, setInput] = useState("")
     const getListTxCode = storeListTxCode(state => state.getListTxCode)
     const [loading, setLoading] = useState(false)
+
+
+    const {showModal, isModalOpen, handleCancel} = useModal()
     // const getDefaultListBalance = storeListAvailableBalance(state => state.getDefaultListBalance)
     // const setSortedListBalance = storeListAvailableBalance(state => state.setSortedListBalance)
     // const getListAllCryptoName = storeListAllCryptoName(state => state.getListAllCryptoName)
@@ -18,8 +23,13 @@ const ApplyCode = () => {
         setLoading(true)
         const response = await apiApplyTxCode(input)
 
+        if (response.data.error === null) {
+
+        }
+
         await getListTxCode()
 
+        showModal()
         setLoading(false)
         setInput("")
     }
@@ -38,6 +48,23 @@ const ApplyCode = () => {
                             className={"w-full !h-full !font-medium"}>
                         Apply
                     </Button>
+                    <Modal title={"The code applied successfully"} onCancel={handleCancel} open={isModalOpen}>
+                        <div className="row mb-6">
+                            <div className="col">
+                                <p className="text-sm">Your account has been topped up for the amount:</p>
+                            </div>
+                        </div>
+                        <div className="row mb-12">
+                            <div className="col">
+                                <p className="text-xl text-green">100.00 EURG</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <Button className="w-full" size={"xl"} onClick={handleCancel}>Done</Button>
+                            </div>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </Form>

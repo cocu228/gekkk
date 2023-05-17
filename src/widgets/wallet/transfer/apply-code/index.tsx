3fb490efec58c9f576698088063c6d9772e0d759
muzhichina <aleksey.muzhichin@gmail.com>
2023-05-17 14:36:20 +0400

    wallet

--- src/widgets/wallet/transfer/apply-code/index.tsx
+++ src/widgets/wallet/transfer/apply-code/index.tsx
@@ -3,34 +3,37 @@ import Button from "@/shared/ui/button/Button";
 import {useState} from "react";
 import TransferTableCode from "@/widgets/wallet/transfer/TransferTableCode";
 import {apiApplyTxCode} from "@/shared/api";
+import {storeListTxCode} from "@/widgets/wallet/transfer/store/list-tx-code";
+import {actionResSuccess} from "@/shared/lib/helpers";
+import {storeListAllCryptoName, storeListAvailableBalance} from "@/shared/store/crypto-assets";
 
 const ApplyCode = () => {
     const [input, setInput] = useState("")
-
-
+    const getListTxCode = storeListTxCode(state => state.getListTxCode)
+    const [loading, setLoading] = useState(false)
+    // const getDefaultListBalance = storeListAvailableBalance(state => state.getDefaultListBalance)
+    // const setSortedListBalance = storeListAvailableBalance(state => state.setSortedListBalance)
+    // const getListAllCryptoName = storeListAllCryptoName(state => state.getListAllCryptoName)
     const onBtnApply = async () => {
+        setLoading(true)
         const response = await apiApplyTxCode(input)
-        console.log(response)
+
+        await getListTxCode()
+
+        setLoading(false)
+        setInput("")
     }
 
     return <>
         <Form onFinish={() => null}>
             <div className="row flex gap-10 mb-10">
                 <div className="col flex items-center w-3/5">
-                    <Form.Item hasFeedback className="mb-0 w-full"
-                               preserve
-                               name={"promo-code"}
-                        // validateStatus={validateStatus(status)}
-                        // rules={[{required: true, ...promoCodeMessage}, promoCodeValidator]}>
-                    >
-                        {/*<Input suffix={false} value={valInput} disabled={loading} onChange={handlerInput}*/}
-                        <Input className={"w-full"} onChange={({target}) => setInput(target.value)}
-                               placeholder={"Enter top up code"} type={"text"}/>
-                    </Form.Item>
+                    <Input className={"w-full"} value={input} disabled={loading}
+                           onChange={({target}) => setInput(target.value)}
+                           placeholder={"Enter top up code"} type={"text"}/>
                 </div>
                 <div className="col h-inherit flex items-center w-2/5">
-                    {/*<Button htmlType={"submit"} disabled={valInput === "" || loading || status === "SUCCESS"}*/}
-                    <Button disabled={input === ""} onClick={onBtnApply} htmlType={"submit"}
+                    <Button disabled={input === "" || loading} onClick={onBtnApply} htmlType={"submit"}
                             size={"xl"}
                             className={"w-full !h-full !font-medium"}>
                         Apply

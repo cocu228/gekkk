21130606d357411fce12ecbd15c9b4267d307a2c
muzhichina <aleksey.muzhichin@gmail.com>
2023-05-24 13:16:44 +0400

    History.tsx

--- src/widgets/sidebar/ui/desktop/index.tsx
+++ src/widgets/sidebar/ui/desktop/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./style.module.scss";
 import Footer from "@/widgets/footer";
 import {useCallback, useContext, useEffect, useRef, useState} from "react";
-import {apiGetRates} from "@/shared/api";
+import {apiGetRates, apiInvestments, IResInvestments} from "@/shared/api";
 import {NavLink} from 'react-router-dom';
 import {ParentClassForCoin, IconCoin} from "@/shared/ui/icons/icon-coin";
 import totalizeAmount from "../../model/totalize-amount";
@@ -14,6 +14,8 @@ import UpdateAmounts from "../../../../features/update-amounts";
 import {helperFilterList} from "@/widgets/sidebar/model/helpers";
 import {CtxCurrencyData} from "@/app/CurrenciesContext";
 import $const from "@/shared/config/coins/constants";
+import {getTypeTitle} from "@/widgets/dashboard/ui/deposit-layout/DepositLayout";
+import {formatDate} from "@/widgets/dashboard/model/helpers";
 
 const SidebarDesktop = () => {
 
@@ -25,6 +27,19 @@ const SidebarDesktop = () => {
     const {sm, md} = useContext(BreakpointsContext)
 
     const NavLinkEvent = useCallback(() => (sm || md) ? toggleSidebar.current(false) : null, [sm, md])
+
+
+    const [investments, setInvestments] = useState<IResInvestments[]>(null)
+
+    useEffect(() => {
+
+        (async () => {
+            const {data} = await apiInvestments();
+            setInvestments(data.result);
+        })()
+
+    }, []);
+
 
     useEffect(() => {
 
@@ -128,9 +143,9 @@ const SidebarDesktop = () => {
                                     <div className="row w-full"><span
                                         className={styles.Sum}>{`${item.availableBalance?.toDecimalPlaces(item.roundPrec)} ${item.currency}`}</span>
                                     </div>
-                                    {/*<div className="row w-full"><span*/}
-                                    {/*    className="text-gray-400 text-sm">{`${item.freezeBalance} (hold)`}</span>*/}
-                                    {/*</div>*/}
+                                    <div className="row w-full"><span
+                                        className="text-gray-400 text-sm">{`${item.lockInBalance} (hold)`}</span>
+                                    </div>
                                 </div>
                             </div>
                         </NavLink>)}
@@ -165,7 +180,27 @@ const SidebarDesktop = () => {
                 </div>
             </NavLink>
             <NavCollapse header={"Current deposit"} id={"deposit"}>
-                <p>Current deposit</p>
+                {investments && investments.map((item, i) =>
+                    <NavLink onClick={NavLinkEvent} to={`${item.dep_type}`} key={item.id}>
+                        <div className={`${styles.Item + " " + ParentClassForCoin}`}>
+                            <div className="col flex items-center pl-4">
+                                <SvgArrow width={14} height={14} className={styles.SvgArrow}/>
+                                <img alt={"DepositIcon.svg"} className={styles.Icon}
+                                     src={"/img/icon/DepositIcon.svg"}/>
+                            </div>
+                            <div className="col flex items-center justify-center flex-col pl-6">
+                                <div className="row w-full mb-1"><span
+                                    className={`${styles.Name} text-gray-400 text-xs`}>Structured deposit, until {formatDate(new Date(item.date_end))}</span>
+                                </div>
+                                <div className="row w-full"><span
+                                    className={styles.Sum}>{item.amount} €</span>
+                                </div>
+                                <div className="row w-full"><span
+                                    className="text-gray-400 text-sm">{getTypeTitle(item.dep_type)}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </NavLink>)}
             </NavCollapse>
         </div>
         {!sm && !md && <Footer textAlight={"text-left"}/>}

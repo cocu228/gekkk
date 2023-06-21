474705c9c16a3b6d07f3b256b32f2e390294844e
rezucos <tsyganok.vasya@yandex.ru>
2023-06-21 15:50:01 +0300

    apiApplyTxCode ts type

--- src/shared/api/various/apply_tx_code.ts
+++ src/shared/api/various/apply_tx_code.ts
@@ -1,8 +1,7 @@
-import $axios from "@/shared/lib/(cs)axios";
-
+import $axios, { $AxiosResponse } from "@/shared/lib/(cs)axios";
 
 export const apiApplyTxCode = (code: string) =>
-    $axios.post('/gek/v1/apply_code', null, {
+    $axios.post<$AxiosResponse<any>>('/gek/v1/apply_code', null, {
         params: {
             code,
         }

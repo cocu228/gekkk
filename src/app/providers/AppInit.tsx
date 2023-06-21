28db663a1f252663c9753bff1a332be803762aba
rezucos <tsyganok.vasya@yandex.ru>
2023-06-21 15:53:22 +0300

    Added private exchange room code url handler

--- src/app/providers/AppInit.tsx
+++ src/app/providers/AppInit.tsx
@@ -1,11 +1,13 @@
-import {AuthProvider, useAuth} from "./AuthRouter";
+import {useRef} from "react";
 import AuthPage from "@/pages/auth";
+import {AuthProvider, useAuth} from "./AuthRouter";
 import RootLayout from "@/app/providers/RootLayout";
+import useRoomCodeHandler from "./useRoomCodeHandler";
 import ErrorsProvider from "@/app/providers/ErrorsProvider";
 
 const AppInit = () => {
-
     const {token} = useAuth();
+    const roomCodeHandlerRef = useRef(useRoomCodeHandler(token));
 
     const content = !token ? <AuthPage/> : <RootLayout/>
 
---
+++ src/app/providers/useRoomCodeHandler.ts
@@ -0,0 +1,33 @@
+import {useEffect} from 'react';
+import {apiApplyTxCode} from '@/shared/api';
+import {uncoverResponse} from '@/shared/lib/helpers';
+import {useNavigate, useSearchParams} from 'react-router-dom';
+
+const useRoomCodeRedirect = (token: string) => {
+    const navigate = useNavigate();
+    const [params] = useSearchParams();
+    const roomCode = params.get("roomCode");
+
+    if (roomCode) {
+        localStorage.setItem("roomCode", roomCode);
+    }
+
+    useEffect(() => {
+        if (token && location.pathname === '/') {
+            const roomCode = localStorage.getItem("roomCode");
+
+            if (!roomCode) return;
+            
+            (async function() {
+                const response = await apiApplyTxCode(roomCode);
+
+                if (response.data.error) return;
+
+                localStorage.removeItem("roomCode");
+                navigate(`/private-room/${uncoverResponse(response)}`);
+            })();
+        }
+    }, [token]);
+};
+
+export default useRoomCodeRedirect;

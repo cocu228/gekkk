fc8fbb734b663f7a742bc0bca98efee442c58eb0
rezucos <tsyganok.vasya@yandex.ru>
2023-06-22 11:01:43 +0300

    operation result rounding

--- src/widgets/exchange/ui/operation-result/OperationResult.tsx
+++ src/widgets/exchange/ui/operation-result/OperationResult.tsx
@@ -1,5 +1,6 @@
-import { useContext } from 'react';
-import { CtxExchangeData } from '../../model/context';
+import {useContext} from 'react';
+import {CtxExchangeData} from '../../model/context';
+import {CtxCurrencyData} from '@/app/CurrenciesContext';
 
 function OperationResult() {
     const {
@@ -7,18 +8,24 @@ function OperationResult() {
         from
     } = useContext(CtxExchangeData);
 
+    const {currencies} = useContext(CtxCurrencyData);
+
     return (
         <div className="flex flex-col gap-2 md:gap-1 font-medium info-box-warning">
             <div className="flex flex-wrap justify-between gap-0.5">
                 <span>You will pay</span>
 
-                <strong>{from.amount && from.currency ? `${from.amount} ${from.currency}` : '-'}</strong>
+                <strong>{!(from.amount && from.currency) ? '-'
+                    : `${+Number(from.amount).toFixed(currencies.get(from.currency).roundPrec)} ${from.currency}`
+                }</strong>
             </div>
             
             <div className="flex flex-wrap justify-between gap-0.5">
                 <span>You will get</span>
 
-                <strong>{to.amount && to.currency ? `${to.amount} ${to.currency}` : '-'}</strong>
+                <strong>{!(to.amount && to.currency) ? '-'
+                    : `${+Number(to.amount).toFixed(currencies.get(to.currency).roundPrec)} ${to.currency}`
+                }</strong>
             </div>
         </div>
     );

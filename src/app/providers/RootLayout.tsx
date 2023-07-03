705ecda981246f504840b3b7711a4a3f39c17e43
aleksmanson <aleksey.muzhichin@gmail.com>
2023-07-03 19:01:29 +0400

    HeaderMenu

--- src/app/providers/RootLayout.tsx
+++ src/app/providers/RootLayout.tsx
@@ -5,7 +5,7 @@ import Main from "@/app/layouts/main/Main";
 import Sidebar from "@/widgets/sidebar/ui/";
 import {memo, useEffect, useState} from 'react';
 import Content from "@/app/layouts/content/Content";
-import {apiGetBalance, apiGetMarketAssets} from '@/shared/api';
+import {apiGetBalance, apiGetInfoClient, apiGetMarketAssets} from '@/shared/api';
 import {CtxRootData, ICtxCurrencyData} from '../RootContext';
 import {actionResSuccess, randomId, uncoverResponse} from '@/shared/lib/helpers';
 import helperCurrenciesGeneration from "@/shared/lib/helperCurrenciesGeneration";
@@ -39,7 +39,10 @@ export default memo(function () {
 
     useEffect(() => {
 
+
+
         (async function () {
+            await apiGetInfoClient()
             // const infoClient = await apiGetInfoClient();
             const walletsResponse = await apiGetBalance();
             const assetsResponse = await apiGetMarketAssets();

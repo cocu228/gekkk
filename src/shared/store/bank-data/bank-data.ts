8dd1bb25438965efdaddc8ea75371db73976d965
rezucos <tsyganok.vasya@yandex.ru>
2023-07-03 11:40:04 +0300

    storeBankData added bankAccounts field

--- src/shared/store/bank-data/bank-data.ts
+++ src/shared/store/bank-data/bank-data.ts
@@ -1,18 +1,29 @@
 import {create} from 'zustand'
 import {devtools} from "zustand/middleware";
-import {apiGetBankData, IBankData} from "@/shared/api";
 import {uncoverArray} from "@/shared/lib/helpers";
+import {apiGetBankData, IBankAccount, IBankData} from "@/shared/api";
 
 export interface IStoreBankData {
     bankData: IBankData;
-    getBankData: () => Promise<IBankData>
+    bankAccounts: Array<IBankAccount>;
+    getBankData: () => Promise<IBankData>;
 }
 
 export const storeBankData = create<IStoreBankData>()(devtools((set) => ({
     bankData: null,
+    bankAccounts: [],
     getBankData: async () => {
         const {data} = await apiGetBankData();
-        set((state) => ({...state, bankData: uncoverArray<IBankData | null>(data)}));
+        const bankData = uncoverArray<IBankData | null>(data);
+        const accounts = bankData.accounts.filter(a =>
+            a.name = bankData.trustedClients.find(tc => tc.clientId === a.clientId)?.name
+        );
+
+        set((state) => ({
+            ...state,
+            bankData: bankData,
+            bankAccounts: accounts
+        }));
         return uncoverArray<IBankData | null>(data);
     },
 })));

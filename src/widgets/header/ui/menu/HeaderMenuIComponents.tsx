bd0506df5fbfc0fdb321a99c683fbf1c0b002dab
rezucos <tsyganok.vasya@yandex.ru>
2023-07-04 14:58:44 +0300

    fix: icon

--- src/widgets/header/ui/menu/HeaderMenuIComponents.tsx
+++ src/widgets/header/ui/menu/HeaderMenuIComponents.tsx
@@ -45,7 +45,9 @@ export const ItemAccount = ({active = false, iban, title}: Partial<{
 export const EmptyAccount = () => {
     return <div className="flex items-center justify-end relative">
         <div className="wrapper mr-2">
-            <img width={32} height={32} src="/img/icon/UserIcon.svg" alt="UserIcon"/>
+            <svg width="32" height="32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" clipRule="evenodd" d="M24.034 17.6c1.23.547 2.302 1.218 3.185 1.994a6.237 6.237 0 012.116 4.687v1.927a3.128 3.128 0 01-3.125 3.125H5.793a3.128 3.128 0 01-3.125-3.125v-1.927c0-1.797.771-3.506 2.115-4.687 1.088-.956 3.173-2.374 6.523-3.11a7.7 7.7 0 01-3.013-6.109c0-4.25 3.458-7.708 7.708-7.708s7.709 3.458 7.709 7.708-3.458 7.708-7.709 7.708c-5.724 0-8.79 2.151-9.843 3.076a4.154 4.154 0 00-1.407 3.122v1.927c0 .575.468 1.042 1.042 1.042H26.21c.574 0 1.041-.467 1.041-1.042v-1.927a4.154 4.154 0 00-1.407-3.122c-.725-.637-1.619-1.194-2.656-1.655a1.042 1.042 0 01.846-1.904zM16 4.75a5.631 5.631 0 00-5.625 5.625A5.631 5.631 0 0016.001 16a5.631 5.631 0 005.625-5.625 5.631 5.631 0 00-5.625-5.625z"/>
+            </svg>
         </div>
         <div className="wrapper grid gap-2">
             <div className="row">

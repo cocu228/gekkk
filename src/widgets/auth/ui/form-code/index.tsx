8e9b285cf3e377cc9913f13baf94ecd327e8689a
rezucos <tsyganok.vasya@yandex.ru>
2023-07-17 11:26:20 +0300

    request code refreshing

--- src/widgets/auth/ui/form-code/index.tsx
+++ src/widgets/auth/ui/form-code/index.tsx
@@ -1,72 +1,79 @@
-import React, {memo, useContext, useEffect, useLayoutEffect, useRef, useState} from 'react';
-import useMask from '@/shared/model/hooks/useMask';
-import {MASK_CODE} from '@/shared/config/mask';
-import Form from '@/shared/ui/form/Form';
-import FormItem from '@/shared/ui/form/form-item/FormItem';
-import {codeMessage} from '@/shared/config/message';
 import {Input} from 'antd';
-import Button from '@/shared/ui/button/Button';
-import {apiRequestCode} from "@/widgets/auth/api";
-import {formatAsNumber} from "@/shared/lib/formatting-helper";
-import {useAuth} from "@/app/providers/AuthRouter";
+import Form from '@/shared/ui/form/Form';
 import {useSessionStorage} from "usehooks-ts";
 import {apiSignIn} from "@/widgets/auth/api/";
-import {BreakpointsContext} from '@/app/providers/BreakpointsProvider';
+import Button from '@/shared/ui/button/Button';
+import {MASK_CODE} from '@/shared/config/mask';
+import {apiRequestCode} from "@/widgets/auth/api";
+import useMask from '@/shared/model/hooks/useMask';
+import {useAuth} from "@/app/providers/AuthRouter";
+import {codeMessage} from '@/shared/config/message';
+import FormItem from '@/shared/ui/form/form-item/FormItem';
 import {storyDisplayStage} from "@/widgets/auth/model/story";
+import {formatAsNumber} from "@/shared/lib/formatting-helper";
+import {BreakpointsContext} from '@/app/providers/BreakpointsProvider';
 import {helperApiRequestCode, helperApiSignIn} from "@/widgets/auth/model/helpers";
-
+import {memo, useContext, useEffect, useLayoutEffect, useRef, useState} from 'react';
 
 const FormCode = memo(() => {
-
-    const {toggleStage} = storyDisplayStage(state => state);
     const {login} = useAuth();
-    const inputRef = useRef(null)
-    const {md} = useContext(BreakpointsContext);
+    const inputRef = useRef(null);
     const {onInput} = useMask(MASK_CODE);
     const [code, setCode] = useState("");
-    const [loading, setLoading] = useState<boolean>(false);
-    const [{phone, sessionId}] = useSessionStorage("session-auth", {phone: "", sessionId: ""});
-    const [, setSessionGlobal] = useSessionStorage("session-global", {});
+    const {md} = useContext(BreakpointsContext);
+    const [timerOn, setTimerOn] = useState(true);
     const [timeLeft, setTimeLeft] = useState(180);
+    const [loading, setLoading] = useState<boolean>(false);
+    const {toggleStage} = storyDisplayStage(state => state);
+    const [, setSessionGlobal] = useSessionStorage("session-global", {});
+    const [{phone, sessionId}] = useSessionStorage("session-auth", {phone: "", sessionId: ""});
 
     useLayoutEffect(() => {
         inputRef.current.focus();
-    }, [])
+    }, []);
+    
     const onFinish = () => {
-
         setLoading(true);
 
         apiRequestCode(phone, formatAsNumber(code), sessionId)
-            .then(res =>
-                helperApiRequestCode(res)
-                    .success(
-                        () => {
-                            setSessionGlobal({sessionId: res.data.sessid})
-                            apiSignIn(formatAsNumber(code), sessionId, phone)
-                                .then(res => helperApiSignIn(res)
-                                    .success(
-                                        () => {
-                                            toggleStage("authorization");
-                                            login(phone, res.data.token)
-                                        })
-                                ).catch(e => {
-                                console.log(e)
+            .then(res => helperApiRequestCode(res)
+                .success(() => {
+                    setSessionGlobal({sessionId: res.data.sessid})
+                    apiSignIn(formatAsNumber(code), sessionId, phone)
+                        .then(res => helperApiSignIn(res)
+                            .success(() => {
+                                toggleStage("authorization");
+                                login(phone, res.data.token);
                             })
-                        }).reject((v) => {
-                    console.log(res)
+                        ).catch(e => {
+                            console.log(e);
+                        });
+                    })
+                .reject(v => {
+                    console.log(res);
                 })
             )
     }
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setTimeLeft((prevTime) => prevTime - 1);
-        }, 1000);
+        let interval;
+        if (timerOn) {
+            interval = setInterval(() => {
+                setTimeLeft((prevTime) => prevTime - 1);
+            }, 1000);
+        }
+        if (timeLeft === 0) {
+            setTimerOn(false);
+        }
 
         return () => clearInterval(interval);
-    }, []);
+    }, [timerOn, timeLeft]);
 
-    if (timeLeft <= 0) toggleStage("authorization");
+    const restartTimer = () => {
+        apiRequestCode(phone);
+        setTimeLeft(180);
+        setTimerOn(true);
+    };
 
     return <Form onFinish={onFinish}>
         <h1 className={`font-extrabold text-center text-gray-600 min-w-[336px] pb-4
@@ -89,7 +96,11 @@ const FormCode = memo(() => {
         </FormItem>
 
         <div className="row text-right -mt-1 mb-12 text-gray-400">
-            You can use the code for {timeLeft} seconds
+            {timeLeft !== 0 ? (
+                <span>You can use the code for {timeLeft} seconds</span>
+            ) : (
+                <a onClick={restartTimer} className='underline'>Resend code</a>
+            )}
         </div>
 
         <div className="row">
@@ -97,6 +108,6 @@ const FormCode = memo(() => {
                     htmlType="submit">Next</Button>
         </div>
     </Form>
-})
+});
 
-export default FormCode
+export default FormCode;

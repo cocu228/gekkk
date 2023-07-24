9ede194131f68be415b41a0502a658af3d934303
aleksmanson <aleksey.muzhichin@gmail.com>
2023-07-24 13:31:11 +0400

    fix query /gek/v1/wallet/create_address

--- src/shared/api/wallet/create-address.ts
+++ src/shared/api/wallet/create-address.ts
@@ -3,7 +3,7 @@ import $axios from "@/shared/lib/(cs)axios";
 
 export const apiCreateAddress = function (token_network: number) {
     return $axios.get('/gek/v1/wallet/create_address', {
-        headers: {
+        params: {
             token_network
         }
     })

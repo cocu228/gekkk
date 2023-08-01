02949d6fb25794da6ca66f32f2f2e4f78f377809
rezucos <tsyganok.vasya@yandex.ru>
2023-08-01 13:08:14 +0300

    dashboard .CardsGrid initial

--- src/shared/ui/cards-grid/CardsGrid.tsx
+++ src/shared/ui/cards-grid/CardsGrid.tsx
@@ -1,18 +1,8 @@
-import React from 'react';
+import styles from './style.module.scss';
 
 function CardsGrid({children}: any) {
     return (
-        <div className="
-            grid
-            grid-cols-4
-            sm:grid-cols-1
-            xl:grid-cols-2
-            xxxl:grid-cols-3
-            justify-items-center
-            gap-4
-            gap-y-10
-            xl:gap-5
-        ">
+        <div className={styles.CardsGrid}>
             {children}
         </div>
     );

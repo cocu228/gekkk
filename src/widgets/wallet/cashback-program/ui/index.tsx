f6183bec5ab289415ccc91c124f04cda0c0e0326
rezucos <tsyganok.vasya@yandex.ru>
2023-08-02 15:56:00 +0300

    cashback refactoring

--- src/widgets/wallet/cashback-program/ui/index.tsx
+++ src/widgets/wallet/cashback-program/ui/index.tsx
@@ -20,14 +20,6 @@ const CashbackProgram = () => {
     const cashbackTemplate = storeInvestTemplates(state => state.cashbackTemplate);
     const updateCashbackInvestment = storeInvestments(state => state.updateCashbackInvestment);
 
-    if (!investment) return <Loader/>;
-
-    const {
-        amount: locked,
-        date_start: startDate,
-        date_end: endDate
-    } = investment;
-
     return (
         <>
             <div className="row mb-10">
@@ -35,29 +27,39 @@ const CashbackProgram = () => {
                     <div className="info-box-description">
                         <div className="row mb-3">
                             <div className="col">
-                                <p className="font-extrabold text-sm">Locking {wallet.currency} tokens gives you
-                                    access to the
-                                    cashback
-                                    of 1% on Gekkard card spending, on
-                                    monthly turnover in euros</p>
-                            </div>
-                        </div>
-                        <div className="row mb-3">
-                            <div className="col flex">
-                                <img className="mr-2" width={20} height={12} src="/img/icon/RightWitheArrowIcon.svg"
-                                     alt="RightWitheArrowIcon"/>
-                                <p className="text-sm">
-                                    up to the amount not exceeding a similar number of
-                                    blocked {wallet.currency} tokens
-
-
+                                <p className="font-extrabold text-sm">
+                                    Locking {wallet.currency} tokens gives you
+                                    access to the cashback of 1% on Gekkard card spending,
+                                    on monthly turnover in euros
                                 </p>
                             </div>
                         </div>
+
+                        <div className="row mb-3">
+                            <div className="col flex">
+                                <img
+                                    className="mr-2"
+                                    width={20} height={12}
+                                    alt="RightWitheArrowIcon"
+                                    src="/img/icon/RightWitheArrowIcon.svg"
+                                />
+
+                                <p className="text-sm">
+                                    up to the amount not exceeding a similar number of
+                                    blocked {wallet.currency} tokens
+                                </p>
+                            </div>
+                        </div>
+
                         <div className="row">
                             <div className="col flex">
-                                <img className="mr-2" width={20} height={12} src="/img/icon/RightWitheArrowIcon.svg"
-                                     alt="RightWitheArrowIcon"/>
+                                <img
+                                    className="mr-2"
+                                    width={20} height={12}
+                                    alt="RightWitheArrowIcon"
+                                    src="/img/icon/RightWitheArrowIcon.svg"
+                                />
+
                                 <p className="text-sm">
                                     cashback is credited once a month at the end of the billing period
                                 </p>
@@ -66,20 +68,25 @@ const CashbackProgram = () => {
                     </div>
                 </div>
             </div>
+
             <div className="row flex flex-wrap mb-6">
                 <div className="col -md:border-r-1 md:mb-5 -md:border-solid -md:border-gray-400 -md:pr-10 md:w-full w-3/5">
                     <CashbackProperties
-                        locked={locked}
+                        locked={investment?.amount ?? 0}
                         amount={amount}
-                        endDate={endDate}
-                        startDate={startDate}
+                        endDate={investment?.date_end ?? new Date()}
+                        startDate={investment?.date_start ?? new Date()}
                         currency={wallet.currency}
                         templateTerm={cashbackTemplate.depo_min_time}
                     />
                 </div>
+
                 <div className="col md:w-full w-2/5 -md:pl-5 md:flex md:justify-center">
-                    <p className="text-fs12 text-gray-500 text-center leading-4 md:text-center md:max-w-[280px]">At the
-                        end of the program term, the blocked {wallet.currency} funds will return to your account</p>
+                    <p className="text-fs12 text-gray-500 text-center leading-4 md:text-center md:max-w-[280px]">
+                        At the end of the program term, the
+                        blocked {wallet.currency} funds will
+                        return to your account
+                    </p>
                 </div>
             </div>
             
@@ -110,8 +117,8 @@ const CashbackProgram = () => {
             <div className="row">
                 <div className="col flex justify-center">
                     <span className="text-fs12 text-gray-500 text-center leading-4">
-                        The period of locking tokens is {cashbackTemplate.depo_min_time} days. At the end of this period
-                        the funds will return to your account.
+                        The period of locking tokens is {cashbackTemplate.depo_min_time} days.
+                        At the end of this period the funds will return to your account.
                     </span>
                 </div>
             </div>
@@ -123,14 +130,14 @@ const CashbackProgram = () => {
                 onCancel={lockConfirmModal.handleCancel}
             >
                 <CashbackProperties
-                    locked={locked}
+                    locked={investment?.amount ?? 0}
                     amount={amount}
-                    startDate={startDate}
+                    startDate={investment?.date_start ?? new Date()}
                     currency={wallet.currency}
                     templateTerm={cashbackTemplate.depo_min_time}
                     endDate={addDays(new Date(), cashbackTemplate.depo_min_time)}
                 />
-                
+
                 <div className="mt-6 md:mt-12">
                     <Button
                         size="xl"
@@ -164,23 +171,33 @@ function CashbackProperties({
     return (
         <>
             <div className="row mb-2">
-                <div className="col"><InlineProperty left={"Start date"} right={formatForCustomer(startDate)}/>
+                <div className="col">
+                    <InlineProperty left={"Start date"} right={formatForCustomer(startDate)}/>
                 </div>
             </div>
+
             <div className="row mb-2">
-                <div className="col"><InlineProperty left={"Term"} right={`${templateTerm} days`}/>
+                <div className="col">
+                    <InlineProperty left={"Term"} right={`${templateTerm} days`}/>
                 </div>
             </div>
+
             <div className="row mb-2">
-                <div className="col"><InlineProperty left={"End date"} right={formatForCustomer(endDate)}/>
+                <div className="col">
+                    <InlineProperty left={"End date"} right={formatForCustomer(endDate)}/>
                 </div>
             </div>
+
             <div className="row">
-                <div className="col"><InlineProperty left={"Locked funds"} right={<>
-                    {locked} {!amount ? null : (
-                        (<span className="text-green">+({amount})</span>)
-                    )} {currency}
-                </>}/>
+                <div className="col">
+                    <InlineProperty
+                        left={"Locked funds"}
+                        right={<>
+                            {locked} {!amount ? null : (
+                                (<span className="text-green">+({amount})</span>)
+                            )} {currency}
+                        </>}
+                    />
                 </div>
             </div>
         </>

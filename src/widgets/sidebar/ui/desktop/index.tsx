9c99c8c0689cf61b9e7c11a903b851023614b843
rezucos <tsyganok.vasya@yandex.ru>
2023-08-09 11:18:21 +0300

    euro svg icon and label

--- src/widgets/sidebar/ui/desktop/index.tsx
+++ src/widgets/sidebar/ui/desktop/index.tsx
@@ -122,7 +122,7 @@ const SidebarDesktop = () => {
                     </div>
                     <div className="col flex items-center justify-center flex-col pl-6">
                         <div className="row text-gray-400 w-full mb-1"><span
-                            className={styles.Name}>Europe</span>
+                            className={styles.Name}>Euro</span>
                         </div>
                         <div className="row w-full">
                             <span className={styles.Sum}>0 EUR</span>

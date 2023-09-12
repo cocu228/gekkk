cab096a7c74097872f112eb8c97b0b15990f1ce2
ReZuCoS <tsyganok.vasya@proton.me>
2023-09-12 06:32:48 +0200

    /payment_sepa request update

---
+++ src/shared/api/bank/payments/payment-sepa.ts
@@ -0,0 +1,57 @@
+import $axios from "@/shared/lib/(cs)axios";
+
+interface IParams {
+    iban: string;
+    account: string;
+    purpose: string;
+    beneficiaryName: string;
+    transferDetails: string;
+    amount: {
+        sum: {
+            value: number;
+            currency: {
+                code: string;
+            }
+        }
+    }
+    
+    // Not used
+    // externalId: string | null;
+    // externalIdOwner: "CALLBACK" | null;
+    // externalOwnerId: string | null;
+    // externalParam: string | null;
+    // originatorData: string | null;
+}
+
+export interface IResCommission {
+    total: number;
+    commission: number;
+    currency: {
+        code: string;
+        label: string; // Not used
+    }
+}
+
+export interface IResErrors {
+    errors: [
+        {
+            code: number;
+            type: string;
+            message: string;
+            properties: Record<string, string>[];
+        }
+    ]
+}
+
+export const apiPaymentSepa = (
+    payment_details: IParams,
+    commission: boolean = false,
+    headers: {
+        'X-Confirmation-Token': string,
+        'X-Confirmation-Code': string
+    } = null
+) => $axios.post<IResCommission | IResErrors>(`/api/v1/payment_sepa${commission ? "/commission" : ""}`, {
+    payment_sepa: payment_details
+}, {
+    headers: headers
+});

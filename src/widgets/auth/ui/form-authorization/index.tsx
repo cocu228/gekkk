04e882d78d6ef80f0f0e2ac3ce5be0ae4aeb8ad2
aleksmanson <aleksey.muzhichin@gmail.com>
2023-09-15 19:45:34 +0400

    auth/quota-exceeded

--- src/widgets/auth/ui/form-authorization/index.tsx
+++ src/widgets/auth/ui/form-authorization/index.tsx
@@ -103,10 +103,13 @@ const FormLoginAccount = memo(() => {
                 console.log(JSON.stringify(error))
 
                 setLoading(false)
+
                 if (error.code === "auth/invalid-phone-number") {
                     localErrorHunter({code: 0, message: "Invalid phone number"})
                 } else if (error.code === "auth/too-many-requests") {
                     localErrorHunter({code: 1, message: "You're seeing this error because of sending too many auth requests from or using one IP address for a given period of time"})
+                } else if (error.code === "auth/quota-exceeded") {
+                    localErrorHunter({code: 1, message: "Exceeded quota for updating account information."})
                 } else if (error.code === "auth/invalid-verification-code") {
                     localErrorHunter({code: 2, message: "Invalid verification code"})
                 }

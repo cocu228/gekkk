29578b3ae1b3606dda9533f7dfc1b20d5c028c72
aleksmanson <aleksey.muzhichin@gmail.com>
2023-09-18 00:18:15 +0400

    change assets EUR rate loading

--- src/app/providers/CurrenciesProvider.tsx
+++ src/app/providers/CurrenciesProvider.tsx
@@ -3,10 +3,13 @@ import {CtxRootData} from '@/processes/RootContext';
 import {ICtxCurrency} from '@/processes/CurrenciesContext';
 import {apiGetBalance, apiGetRates} from '@/shared/api';
 import {
-    actionResSuccess, randomId,
+    actionResSuccess, isNull, randomId,
     uncoverResponse
 } from '@/shared/lib/helpers';
-import helperCurrenciesGeneration from "@/shared/lib/helper-currencies-generation";
+import {
+    initEmptyCurrenciesCollection,
+    walletsGeneration
+} from "@/shared/lib/helpers-currencies-provider";
 import Decimal from 'decimal.js';
 import {CtxCurrencies} from "@/processes/CurrenciesContext";
 import Loader from "@/shared/ui/loader";
@@ -20,11 +23,8 @@ export default memo(function ({children}: { children: React.ReactNode }): JSX.El
     const getAssets = storeAssets(state => state.getAssets)
     const assets = storeAssets(state => state.assets)
 
-    const [{
-        currencies,
-        totalAmount
-    }, setState] = useState({
-        currencies: new Map<string, ICtxCurrency>(),
+    const [state, setState] = useState({
+        currencies: null,
         totalAmount: {
             EUR: null,
             BTC: null,
@@ -39,15 +39,18 @@ export default memo(function ({children}: { children: React.ReactNode }): JSX.El
             const walletsResponse = await apiGetBalance();
             const assetsResponse = assets ? assets : await getAssets()
 
-            let currencies: Map<string, ICtxCurrency> | undefined
+
+            let currencies: Map<string, ICtxCurrency>
 
             actionResSuccess(walletsResponse)
                 .success(async function () {
 
-                    currencies = helperCurrenciesGeneration(
-                        assetsResponse,
-                        uncoverResponse(walletsResponse),
-                    )
+                    currencies = isNull(state.currencies) ? initEmptyCurrenciesCollection(assetsResponse)
+                        : state.currencies
+
+                    currencies = walletsGeneration(currencies, uncoverResponse(walletsResponse))
+
+
 
                     setState(prev => ({
                                 ...prev,
@@ -62,8 +65,7 @@ export default memo(function ({children}: { children: React.ReactNode }): JSX.El
 // поэтому его значения не дожидаются выполнения полного цикла CtxCurrency
                     const eurResponse = await apiGetBalance('EUR');
 
-                    currencies.set("EUR", new ICtxCurrency(assetsResponse.find(it => it.code === "EUR"),
-                        uncoverResponse(eurResponse)));
+                    currencies = walletsGeneration(currencies, uncoverResponse(eurResponse))
 
                     setState(prev => ({
                         ...prev, currencies,
@@ -83,12 +85,12 @@ export default memo(function ({children}: { children: React.ReactNode }): JSX.El
 
     useEffect(() => {
 
-        if (currencies.size !== 0) (async () => {
+        if (state.currencies !== null) (async () => {
             const ratesEUR = await apiGetRates()
             const ratesBTC = await apiGetRates("BTC")
 
-            const valueEUR: Decimal = totalizeAmount(currencies, ratesEUR.data.result)
-            const valueBTC: Decimal = totalizeAmount(currencies, ratesBTC.data.result)
+            const valueEUR: Decimal = totalizeAmount(state.currencies, ratesEUR.data.result)
+            const valueBTC: Decimal = totalizeAmount(state.currencies, ratesBTC.data.result)
 
             setState(prev => ({
                 ...prev,
@@ -100,13 +102,13 @@ export default memo(function ({children}: { children: React.ReactNode }): JSX.El
             }))
         })();
 
-    }, [totalAmount.refreshKey]);
+    }, [state.totalAmount.refreshKey]);
 
     return <CtxCurrencies.Provider value={{
-        currencies,
-        totalAmount
+        currencies: state.currencies,
+        totalAmount: state.totalAmount
     }}>
-        {currencies.size === 0 ? <Loader/> : children}
+        {state.currencies === null ? <Loader/> : children}
     </CtxCurrencies.Provider>
 });
 
---
+++ src/shared/lib/helpers-currencies-provider.ts
@@ -0,0 +1,26 @@
+import constants from "../config/coins/constants";
+import {ICtxCurrency} from "@/processes/CurrenciesContext";
+import {IResBalance, IResMarketAsset} from "../api";
+import Decimal from "decimal.js";
+
+export const initEmptyCurrenciesCollection = (assets: IResMarketAsset[]): Map<string, ICtxCurrency> => {
+    const currencies = new Map();
+    assets.forEach(asset => currencies.set(asset.code, new ICtxCurrency(asset, null)))
+    return currencies;
+}
+
+
+export const walletsGeneration = (currencies: Map<string, ICtxCurrency>, wallets: IResBalance[]): Map<string, ICtxCurrency> => {
+
+    wallets.forEach(wallet => {
+        currencies.set(wallet.currency, {
+            ...currencies.get(wallet.currency),
+            lockOrders: wallet?.lock_orders,
+            lockInBalance: wallet?.lock_in_balance,
+            lockOutBalance: wallet?.lock_out_balance,
+            availableBalance: (wallet && wallet.free_balance) ? new Decimal(wallet.free_balance) : null
+        })
+    })
+
+    return currencies;
+}

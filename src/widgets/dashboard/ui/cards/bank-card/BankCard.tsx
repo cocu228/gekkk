58d10764b034651f79fb0845993177648e156044
ReZuCoS <tsyganok.vasya@proton.me>
2023-09-29 09:18:29 +0200

    bank-cards status

--- src/widgets/dashboard/ui/cards/bank-card/BankCard.tsx
+++ src/widgets/dashboard/ui/cards/bank-card/BankCard.tsx
@@ -1,4 +1,5 @@
 import styles from './style.module.scss';
+import {CardStatusDescriptions} from "@/shared/store/bank-cards/bankCards";
 
 interface CardData {
     expiresAt: string;
@@ -13,60 +14,71 @@ interface CardData {
 
 const BankCard = ({cardNumber, status, expiresAt, holderName, className}: CardData) => {
     return (
-        <div className={`
-                ${styles.BankCard}
-                ${className}
-                grid-rows-6
-            `}
-        >
-            <img
-                src='/img/payment-card/gekkard-logo.svg'
-                className='h-[37px]'
-            />
-
-            <div className='flex justify-end'>
-                <img
-                    src='/img/payment-card/contactless-icon.svg'
-                    className='h-[22px] mr-5'
-                />
-            </div>
-
-            <img
-                src='/img/payment-card/chip-icon.svg'
-                className='ml-2'
-            />
-
+        <div>
+            {status !== 'ACTIVE' && (
+                <div className='flex absolute w-full h-full items-center justify-center'>
+                    <div className='mb-4 bg-black bg-opacity-75 rounded-[6px] p-2'>
+                        <span className='font-bold select-none text-red-800 leading-5 text-[18px]'>{CardStatusDescriptions[status]}</span>
+                    </div>
+                </div>
+            )}
+            
             <div
                 className={`
+                    ${styles.BankCard}
+                    ${className}
+                    grid-rows-6
+                `}
+            >
+                <img
+                    src='/img/payment-card/gekkard-logo.svg'
+                    className='h-[37px]'
+                />
+
+                <div className='flex justify-end'>
+                    <img
+                        src='/img/payment-card/contactless-icon.svg'
+                        className='h-[22px] mr-5'
+                    />
+                </div>
+
+                <img
+                    src='/img/payment-card/chip-icon.svg'
+                    className='ml-2'
+                />
+
+                <div
+                    className={`
                     ${styles.CardNumber}
                     mt-2
                     mb-2
                     ml-2
                     text-[18px]
                 `}
-            >
-                {cardNumber}
-            </div>
-
-            <div className='text-[#fefefe] text-[6px] font-semibold'>
-                VALID THRU
-            </div>
-
-            <div className={styles.CardBottom}>
-                <div>
-                    <div className={styles.CardData}>
-                        {expiresAt}
-                    </div>
-
-                    <div className={styles.CardData}>
-                        {holderName}
-                    </div>
+                >
+                    {cardNumber}
                 </div>
 
-                <img
-                    src='/img/payment-card/mastercard-logo.svg'
-                    className='ml-2 mt-2 place-self-end'
-                />
+                <div className='text-[#fefefe] text-[6px] font-semibold'>
+                    VALID THRU
+                </div>
+
+                <div className={styles.CardBottom}>
+                    <div>
+                        <div className={styles.CardData}>
+                            {expiresAt}
+                        </div>
+
+                        <div className={styles.CardData}>
+                            {holderName}
+                        </div>
+                    </div>
+
+                    <img
+                        src='/img/payment-card/mastercard-logo.svg'
+                        className='ml-2 mt-2 place-self-end'
+                    />
+                </div>
             </div>
         </div>
     )

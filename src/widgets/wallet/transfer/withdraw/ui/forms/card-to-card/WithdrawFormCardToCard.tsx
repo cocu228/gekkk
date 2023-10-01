26f701b26be414c3a9764f6c882a73567fdf0725
ReZuCoS <tsyganok.vasya@proton.me>
2023-10-01 23:40:21 +0200

    default selected card

--- src/widgets/wallet/transfer/withdraw/ui/forms/card-to-card/WithdrawFormCardToCard.tsx
+++ src/widgets/wallet/transfer/withdraw/ui/forms/card-to-card/WithdrawFormCardToCard.tsx
@@ -1,5 +1,5 @@
 import {Select} from "antd";
-import {useContext, useState} from 'react';
+import Loader from "@/shared/ui/loader";
 import Modal from "@/shared/ui/modal/Modal";
 import Input from "@/shared/ui/input/Input";
 import TextArea from "antd/es/input/TextArea";
@@ -7,6 +7,7 @@ import Button from "@/shared/ui/button/Button";
 import useMask from "@/shared/model/hooks/useMask";
 import useModal from "@/shared/model/hooks/useModal";
 import {IconCoin} from "@/shared/ui/icons/icon-coin";
+import {useContext, useEffect, useState} from 'react';
 import {MASK_BANK_CARD_NUMBER} from "@/shared/config/mask";
 import SearchSelect from "@/shared/ui/search-select/SearchSelect";
 import {storeBankCards} from "@/shared/store/bank-cards/bankCards";
@@ -42,153 +43,161 @@ const WithdrawFormCardToCard = () => {
     }
     
     const isValidated = () => Object.keys(inputs).every(i => {
-        if (i === 'comment') return true;
         if (!inputs[i]) return false;
         if (i === 'cardNumber') return inputs[i].length === 19;
         
         return inputs[i].length > 0;
     });
     
-    return (<div className="wrapper">
-        <div className="row mb-8 w-full">
-            <div className="col">
-                <div className="row mb-2">
-                    <div className="col">
-                        <span>From card</span>
+    useEffect(() => {
+        setInputs(() => ({
+            ...inputs,
+            selectedCard: cards ? cards[0].cardId : null
+        }));
+    }, [cards]);
+    
+    return !cards ? <Loader className={'relative'}/> : (
+        <div className="wrapper">
+            <div className="row mb-8 w-full">
+                <div className="col">
+                    <div className="row mb-2">
+                        <div className="col">
+                            <span>From card</span>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <SearchSelect
-                            placeholder={<span className='font-normal text-gray-400'>
-                                Choose source card
-                            </span>}
-                            prefixIcon={inputs.selectedCard && <IconCoin code={'EUR'}/>}
-                            onChange={val => {
-                                setInputs(() => ({
-                                    ...inputs,
-                                    selectedCard: val
-                                }))
-                            }}
-                        >
-                            {cards?.filter(c => c.cardStatus === "ACTIVE")
-                                .map(c => (
-                                    <Option
-                                        value={c.cardId}
-                                        label={formatCardNumber(c.displayPan)}
-                                    >
-                                        <div>{formatCardNumber(c.displayPan)}</div>
-                                    </Option>
-                                ))
-                            }
-                        </SearchSelect>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="row mb-8 w-full">
-            <div className="col">
-                <div className="row mb-2">
-                    <div className="col">
-                        <span>Card number</span>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <Input
-                            placeholder={"Enter recipient's card number"}
-                            type={'text'}
-                            onInput={onCardNumberInput}
-                            onChange={({target}) => {
-                                setInputs(() => ({
-                                    ...inputs,
-                                    cardNumber: target.value
-                                }));
-                            }}
-                        />
+                    <div className="row">
+                        <div className="col">
+                            <SearchSelect
+                                value={inputs.selectedCard}
+                                placeholder={<span className='font-normal text-gray-400'>
+                                    Choose source card
+                                </span>}
+                                prefixIcon={<IconCoin code={'EUR'}/>}
+                                onChange={(val: string) => {
+                                    setInputs(() => ({
+                                        ...inputs,
+                                        selectedCard: val
+                                    }))
+                                }}
+                            >
+                                {cards?.filter(c => c.cardStatus === "ACTIVE")
+                                    .map(c => (
+                                        <Option
+                                            value={c.cardId}
+                                            label={formatCardNumber(c.displayPan)}
+                                        >
+                                            <div>{formatCardNumber(c.displayPan)}</div>
+                                        </Option>
+                                    ))
+                                }
+                            </SearchSelect>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className="row mb-8 w-full">
-            <div className="col">
-                <div className="row mb-2">
-                    <div className="col">
-                        <span>Cardholder name</span>
+            <div className="row mb-8 w-full">
+                <div className="col">
+                    <div className="row mb-2">
+                        <div className="col">
+                            <span>Card number</span>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <Input value={inputs.cardholderName} onChange={onInput}
-                               placeholder={"Enter cardholder name"}
-                               name={"cardholderName"}/>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="row mb-8 w-full">
-            <div className="col">
-                <div className="row mb-2">
-                    <div className="col">
-                        <span>Comment (optional)</span>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col flex items-center">
-                        <TextArea value={inputs.comment}
-                                  name={"comment"}
-                                  onChange={onInput}
-                                  placeholder={"Enter the comment"}
-                                  style={{
-                                      minHeight: 100
-                                  }}
-                        />
+                    <div className="row">
+                        <div className="col">
+                            <Input
+                                placeholder={"Enter recipient's card number"}
+                                type={'text'}
+                                onInput={onCardNumberInput}
+                                onChange={({target}) => {
+                                    setInputs(() => ({
+                                        ...inputs,
+                                        cardNumber: target.value.replaceAll(' ', '')
+                                    }));
+                                }}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className="row mb-8 w-full">
-            <div className="col">
-                <div className="row mb-2">
-                    <div className="col">
-                        <span>Amount</span>
+            <div className="row mb-8 w-full">
+                <div className="col">
+                    <div className="row mb-2">
+                        <div className="col">
+                            <span>Cardholder name</span>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <InputCurrency.Validator value={inputs.amount} validators={[]}>
-                            <InputCurrency
-                                onChange={(v: unknown) => setInputs(() => ({
-                                    ...inputs,
-                                    amount: v as string
-                                }))}
-                                value={inputs.amount}
-                                currency={currency.$const}/>
-                        </InputCurrency.Validator>
+                    <div className="row">
+                        <div className="col">
+                            <Input value={inputs.cardholderName} onChange={onInput}
+                                   placeholder={"Enter cardholder name"}
+                                   name={"cardholderName"}/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        
-        <Modal width={450} title="Transfer confirmation"
-               onCancel={handleCancel}
-               open={isModalOpen}
-        >
-            <WithdrawConfirmCardToCard {...inputs} handleCancel={handleCancel}/>
-        </Modal>
-        
-        <div className="row mb-8 w-full">
-            <div className="col">
-                <Button
-                    size={"xl"}
-                    className="w-full"
-                    onClick={showModal}
-                    disabled={!isValidated}
-                >Withdraw</Button>
+            <div className="row mb-8 w-full">
+                <div className="col">
+                    <div className="row mb-2">
+                        <div className="col">
+                            <span>Comment</span>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col flex items-center">
+                            <TextArea value={inputs.comment}
+                                      name={"comment"}
+                                      onChange={onInput}
+                                      placeholder={"Enter the comment"}
+                                      style={{
+                                          minHeight: 100
+                                      }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row mb-8 w-full">
+                <div className="col">
+                    <div className="row mb-2">
+                        <div className="col">
+                            <span>Amount</span>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <InputCurrency.Validator value={inputs.amount} validators={[]}>
+                                <InputCurrency
+                                    onChange={(v: unknown) => setInputs(() => ({
+                                        ...inputs,
+                                        amount: v as string
+                                    }))}
+                                    value={inputs.amount}
+                                    currency={currency.$const}/>
+                            </InputCurrency.Validator>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <Modal width={450} title="Transfer confirmation"
+                   onCancel={handleCancel}
+                   open={isModalOpen}
+            >
+                <WithdrawConfirmCardToCard {...inputs} handleCancel={handleCancel}/>
+            </Modal>
+            
+            <div className="row mb-8 w-full">
+                <div className="col">
+                    <Button
+                        size={"xl"}
+                        className="w-full"
+                        onClick={showModal}
+                        disabled={!isValidated}
+                    >Withdraw</Button>
+                </div>
             </div>
         </div>
-    </div>)
-
+    )
 };
 
 export default WithdrawFormCardToCard;

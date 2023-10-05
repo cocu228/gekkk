c9cd28f706ca5aa7d97982c2e6cb096b49343e07
ReZuCoS <tsyganok.vasya@proton.me>
2023-10-05 17:40:53 +0200

    fix: IResHistoryTransactions amount type set to string

--- src/shared/api/wallet/get-history-transactions.ts
+++ src/shared/api/wallet/get-history-transactions.ts
@@ -1,19 +1,19 @@
-import $axios, { $AxiosResponse } from "@/shared/lib/(cs)axios";
+import $axios, {$AxiosResponse} from "@/shared/lib/(cs)axios";
 
 export interface IResHistoryTransactions {
-    "status_text": string,
-    "id_transaction": string,
-    "datetime": string,
-    "currency": string,
-    "tx_type_text":string,
-    "status": number,
-    "amount": number,
-    "fee": number,
-    "is_income": boolean,
-    "balance": number,
-    "partner_info": string,
-    "tag": string,
-    "tx_type": number
+    status_text: string;
+    id_transaction: string;
+    datetime: string;
+    currency: string;
+    tx_type_text:string;
+    status: number;
+    amount: string;
+    fee: number;
+    is_income: boolean;
+    balance: number;
+    partner_info: string;
+    tag: string;
+    tx_type: number;
 }
 
 export const apiHistoryTransactions = (start?: string | number, end?: string, currencies?: string[], tx_types?: Array<number> | null, from_tx_id?: string, limit?: number) =>

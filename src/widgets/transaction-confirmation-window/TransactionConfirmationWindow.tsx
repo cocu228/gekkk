e5f71f2b618261f6e71fbe2ef1d18673beca7e6c
aleksmanson <aleksey.muzhichin@gmail.com>
2023-10-15 23:35:14 +0400

    TransactionConfirmationWindow

--- src/widgets/transaction-confirmation-window/TransactionConfirmationWindow.tsx
+++ src/widgets/transaction-confirmation-window/TransactionConfirmationWindow.tsx
@@ -2,11 +2,11 @@ import {CtxNeedConfirm} from "@/processes/errors-provider-context";
 import {useContext} from "react";
 
 export const TransactionConfirmationWindow = () => {
-    const data = useContext(CtxNeedConfirm)
+    const {data, setSuccess} = useContext(CtxNeedConfirm)
 
 
-    console.log(data.status)
-    console.log(data.response)
+    console.log(data)
+    console.log(setSuccess)
 
     return <>
 

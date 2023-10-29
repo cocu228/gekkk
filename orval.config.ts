f19028be838d4d2da32ae01dc4d2d61f7a7028af
ReZuCoS <tsyganok.vasya@proton.me>
2023-10-29 23:02:44 +0100

    Orval model generator

---
+++ orval.config.ts
@@ -0,0 +1,30 @@
+﻿import {defineConfig} from 'orval';
+
+export default defineConfig({
+	gek: {
+		input: {
+			target: 'https://api-dev.gekkoin.com/gek/swagger/v1/swagger.json',
+		},
+		output: {
+			target: './src/shared/api/(gen)new/newApi.ts',
+			schemas: './src/shared/api/(gen)new/model',
+			client: 'axios-functions',
+			override: {
+				transformer: (outputSchema) => {
+					const methodName = outputSchema.operationName
+						.replace(/(get|post|put|delete|patch)/i, 'api')
+						.replace(/(Wallet|Invest|Market|Referral)/i, '')
+						.replace('GekV1', '');
+					
+					return ({
+						...outputSchema,
+						operationName: methodName
+					})
+				},
+				mutator: {
+					path: './src/shared/lib/(cs)axios-new.ts',
+				},
+			}
+		}
+	},
+});
---
+++ src/shared/lib/(cs)axios-new.ts
@@ -0,0 +1,67 @@
+import axios, {AxiosRequestConfig, AxiosResponse} from 'axios';
+import {getCookieData} from "@/shared/lib/helpers";
+
+export type $AxiosError = {
+    code: number;
+    message: string;
+}
+
+export type $AxiosResponse<T> = {
+    id: number;
+    error: $AxiosError;
+    result: T;
+}
+
+const {MODE} = import.meta.env
+const API_URL_MODE = import.meta.env[`VITE_API_URL_${MODE}`]
+
+const sessionHeader = () => {
+
+
+    const {phone, token, tokenHeaderName} = getCookieData<{ phone: string, token: string, tokenHeaderName: string }>()
+
+    const keys = token ? {
+        'Authorization': phone,
+        [tokenHeaderName]: token,
+    } : {}
+
+    return {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'applicationId': 'GEKKARD',
+        'productId': "GEKKARD",
+        ...keys
+    }
+}
+
+const AXIOS_INSTANCE = axios.create({
+    withCredentials: true,
+    headers: sessionHeader(),
+    paramsSerializer: {
+        indexes: null // by default: false
+    },
+    responseType: 'json',
+    baseURL: !!API_URL_MODE ? API_URL_MODE : window.location.origin
+});
+
+export const $axios = <T>(
+    config: AxiosRequestConfig,
+    options?: AxiosRequestConfig,
+): Promise<AxiosResponse<T>> => {
+    const source = axios.CancelToken.source();
+    
+    // Add .then(({data}) => data) to remove basic AxiosResponse class
+    const promise = AXIOS_INSTANCE({
+        ...config,
+        ...options,
+        cancelToken: source.token,
+    });
+    
+    // @ts-ignore
+    promise.cancel = () => {
+        source.cancel();
+    };
+    
+    return promise;
+};
+
+export default $axios;

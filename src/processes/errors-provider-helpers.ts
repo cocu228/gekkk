e5ff4297373e6024a15686fffea1f5cc997e51df
rezucos <tsyganok.vasya@proton.me>
2023-11-09 03:18:42 +0100

    fix: 10001 (wallet generation) error

--- src/processes/errors-provider-helpers.ts
+++ src/processes/errors-provider-helpers.ts
@@ -3,7 +3,7 @@ import {uncoverArray} from "@/shared/lib/helpers";
 import {IStateErrorProvider, TResponseErrorProvider} from "@/processes/errors-provider-types";
 
 export const skipList = [
-    10006, 10007, 10016, 10024, 10039, 10047, 10064, 10035, 10054, 10065
+    10001, 10006, 10007, 10016, 10024, 10039, 10047, 10064, 10035, 10054, 10065
 ]
 
 export function hunterErrorStatus(error) {

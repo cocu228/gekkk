1d4195de3527b1c2c6ee3dd1239a3ca05a6864a5
rezucos <tsyganok.vasya@proton.me>
2023-11-12 21:42:09 +0100

    fix: header menu

--- src/widgets/header/ui/desktop/desktop.tsx
+++ src/widgets/header/ui/desktop/desktop.tsx
@@ -1,80 +1,18 @@
+import {useContext} from "react";
 import styles from "./style.module.scss";
 import {useNavigate} from "react-router-dom";
-import {useTranslation} from "react-i18next";
 import {useAuth} from "@/app/providers/AuthRouter";
 import {CtxRootData} from "@/processes/RootContext";
 import SvgSchema from "@/shared/ui/icons/IconSchema";
 import {getFormattedIBAN} from "@/shared/lib/helpers";
 import HeaderMenu from "@/widgets/header/ui/menu/HeaderMenu";
-import {TOnActionParams} from "@/widgets/header/model/types";
 import {AccountRights} from "@/shared/config/account-rights";
-import {getDefaultItems} from "../../model/header-menu-items";
-import {storeAccounts} from "@/shared/store/accounts/accounts";
-import {memo, useContext, useEffect, useMemo, useState} from "react";
 import {LocalizationMenu} from "@/widgets/header/ui/LocalizationMenu";
-import {ItemOrganization, ItemAccount} from "@/widgets/header/ui/menu/HeaderMenuIComponents";
 
-const HeaderDesktop = memo((props) => {
-
+const HeaderDesktop = ({items, actions}) => {
     const {logout} = useAuth();
     const navigate = useNavigate();
-    const {t, i18n} = useTranslation();
-    const {account, setAccount} = useContext(CtxRootData);
-    const accounts = storeAccounts(state => state.accounts);
-    const defaultMenuItems = useMemo(() => getDefaultItems(t), [i18n.language]);
-    const [items, setItems] = useState(defaultMenuItems);
-
-    const actionsForMenuFunctions: TOnActionParams = useMemo(() => [
-        {type: "logout", action: () => logout()},
-        {type: "link", action: (value) => navigate(value.toString())},
-        {
-            type: "change-account", action: (value) => {
-                navigate("/");
-                setAccount(value.toString());
-                window.location.reload();
-            }
-        }
-    ], []);
-
-    useEffect(() => {
-        if (!account.rights) return;
-
-        let newItems = [...defaultMenuItems]
-
-        accounts
-            .sort(acc => acc.rights[AccountRights.IsJuridical] ? -1 : 1)
-            .forEach(acc => {
-                newItems.unshift({
-                    id: acc.number,
-                    item: acc.rights[AccountRights.IsJuridical] ? (
-                        <ItemOrganization
-                        number={getFormattedIBAN(acc.number)}
-                        name={acc.name}
-                        active={account.number === acc.number}
-                        />
-                    ) : (
-                        <ItemAccount
-                            number={getFormattedIBAN(acc.number)}
-                            name={acc.name}
-                            active={account.number === acc.number}
-                        />
-                    ),
-                    action: {
-                        type: "change-account",
-                        value: acc.number,
-                    },
-                    style: {
-                        backgroundColor: "var(--color-gray-300)"
-                    }
-                })
-            })
-
-        setItems(!account.rights[AccountRights.IsJuridical]
-            ? newItems
-            : newItems.filter(i => !(i.id === 'investPlatform' || i.id === 'partnership'))
-        );
-
-    }, [account.rights, defaultMenuItems]);
+    const {account} = useContext(CtxRootData);
 
     return <>
         <header className={`flex ${styles.Header}`}>
@@ -83,18 +21,21 @@ const HeaderDesktop = memo((props) => {
                     <img src="/img/logo.svg" width={165} height={55} alt="logo"/>
                 </a>
             </div>
+            
             <div className="flex ml-auto items-center">
                 <LocalizationMenu/>
             </div>
+            
             <HeaderMenu
                 items={items}
-                actions={actionsForMenuFunctions}
+                actions={actions}
             >
                 <div className="flex items-center justify-end" data-testid="HeaderMenuContainer">
                     <div className="wrapper mr-2">
                         {account.rights[AccountRights.IsJuridical] ? <SvgSchema width={32} height={22}/> :
                             <img width={32} height={32} src="/img/icon/UserIcon.svg" alt="UserIcon"/>}
                     </div>
+                    
                     {account.number && <div className="wrapper">
                         <div className="row">
                             <span className="text-sm font-bold">{account.name}</span>
@@ -106,6 +47,7 @@ const HeaderDesktop = memo((props) => {
                             </span>
                         </div>
                     </div>}
+                    
                     <img
                         className="inline-flex mb-3"
                         src="/img/icon/DropdownTriangleIcon.svg"
@@ -113,6 +55,7 @@ const HeaderDesktop = memo((props) => {
                     />
                 </div>
             </HeaderMenu>
+            
             <button onClick={logout}>
                 <div className="flex items-center justify-end ml-10" data-testid="Logout">
                     <img width={26} height={26} src="/img/icon/LogoutIcon.svg" alt="UserIcon"/>
@@ -120,6 +63,6 @@ const HeaderDesktop = memo((props) => {
             </button>
         </header>
     </>
-})
+}
 
 export default HeaderDesktop;
--- src/widgets/header/ui/mobile/mobile.tsx
+++ src/widgets/header/ui/mobile/mobile.tsx
@@ -1,50 +1,47 @@
-import styles from "./style.module.scss"
-import {storyToggleSidebar} from "@/widgets/sidebar/model/story";
-import {useMemo, useRef} from "react";
+import styles from "./style.module.scss";
+import {useContext, useRef} from "react";
+import {CtxRootData} from "@/processes/RootContext";
+import SvgSchema from "@/shared/ui/icons/IconSchema";
 import HeaderMenu from "@/widgets/header/ui/menu/HeaderMenu";
-import {getDefaultItems, HeaderMenuItems} from "@/widgets/header/model/header-menu-items";
-import {TOnActionParams} from "@/widgets/header/model/types";
-import {useAuth} from "@/app/providers/AuthRouter";
-import {useNavigate} from "react-router-dom";
+import {AccountRights} from "@/shared/config/account-rights";
+import {storyToggleSidebar} from "@/widgets/sidebar/model/story";
 import {LocalizationMenu} from "@/widgets/header/ui/LocalizationMenu";
-import {useTranslation} from "react-i18next";
 
-const HeaderMobile = () => {
-    const {logout} = useAuth();
-    const {t} = useTranslation();
-    const navigate = useNavigate();
-    const toggleSidebar = useRef(storyToggleSidebar(state => state.toggle))
-    const isOpen = storyToggleSidebar(state => state.isOpen)
-    const items = useMemo(() => new HeaderMenuItems(getDefaultItems(t)), [])
-    const actionsForMenuFunctions: TOnActionParams = useMemo(() => [
-        {type: "link", action: (value) => navigate(value)},
-        {type: "change-account", action: async (value) => await navigate(value)},
-        {type: "logout", action: () => logout()}
-    ], [])
+const HeaderMobile = ({items, actions}) => {
+    const {account} = useContext(CtxRootData);
+    const isOpen = storyToggleSidebar(state => state.isOpen);
+    const toggleSidebar = useRef(storyToggleSidebar(state => state.toggle));
 
     return <>
         <header className="flex justify-between bg-white">
             <div className="flex items-center">
                 <button onClick={() => toggleSidebar.current(!isOpen)}
                         className={`${styles.NavBtn} ${isOpen ? "active" : ""}`}/>
+                
                 <a href="/">
                     <img style={{objectFit: "contain"}} src="/img/logo.svg" width={72}
                          height={24} alt="logo"/>
                 </a>
             </div>
+            
             <div className="wrapper flex flex-row flex-nowrap">
-                <HeaderMenu items={items.get()} actions={actionsForMenuFunctions}>
-                    <div className="wrapper flex justify-end w-[180px]">
-                        <button className="arrow-down-xs"></button>
+                <HeaderMenu items={items} actions={actions}>
+                    <div className="flex items-center justify-end" data-testid="HeaderMenuContainer">
+                        <div className="wrapper flex justify-end">
+                            {account.rights[AccountRights.IsJuridical] ? <SvgSchema width={32} height={22}/> :
+                                <img width={32} height={32} src="/img/icon/UserIcon.svg" alt="UserIcon"/>}
+                            
+                            <button className="arrow-down-xs"></button>
+                        </div>
                     </div>
                 </HeaderMenu>
-                <div className="wrapper w-[32px] flex pr-4">
+                
+                <div className="wrapper w-[32px] ml-2 flex pr-4">
                     <LocalizationMenu/>
                 </div>
             </div>
         </header>
     </>
-
 }
 
-export default HeaderMobile
+export default HeaderMobile;

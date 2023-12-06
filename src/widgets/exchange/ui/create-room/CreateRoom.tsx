9b4e05bd7ab30d871893dbaae013d964d08fe8d8
rezucos <tsyganok.vasya@proton.me>
2023-12-06 18:11:26 +0100

    fix: room creation

--- src/widgets/exchange/ui/create-room/CreateRoom.tsx
+++ src/widgets/exchange/ui/create-room/CreateRoom.tsx
@@ -1,3 +1,4 @@
+import {t} from 'i18next';
 import Loader from '@/shared/ui/loader';
 import {apiCreateRoom} from '@/shared/api';
 import {useContext, useState} from 'react';
@@ -5,14 +6,15 @@ import Input from '@/shared/ui/input/Input';
 import Button from '@/shared/ui/button/Button';
 import {CtxExchangeData} from '../../model/context';
 import Checkbox from '@/shared/ui/checkbox/Checkbox';
+import UseError from "@/shared/model/hooks/useError";
 import {CurrencyFlags} from '@/shared/config/mask-currency-flags';
 import ModalInfoText from '@/shared/ui/modal/modal-info-text/ModalInfoText';
 import TokenSelect from '@/shared/ui/search-select/token-select/TokenSelect';
-import { t } from 'i18next';
 
 function CreateRoom() {
     const [loading, setLoading] = useState<boolean>(false);
-
+    const [localErrorHunter, , localErrorInfoBox] = UseError();
+    
     const {
         to,
         from,
@@ -20,13 +22,13 @@ function CreateRoom() {
         onToCurrencyChange,
         onFromCurrencyChange
     } = useContext(CtxExchangeData);
-
+    
     return <>
         <div className={loading ? '!collapse' : ''}>
             <ModalInfoText>
                 {t("exchange.private_room_allows")}
             </ModalInfoText>
-
+            
             <div className="mt-4">
                 <label className="inline-flex mb-1 text-sm font-medium" htmlFor="sell-token">{t("exchange.from")}</label>
                 <TokenSelect
@@ -40,7 +42,7 @@ function CreateRoom() {
                     ]}
                 />
             </div>
-
+            
             <div className="mt-2">
                 <label className="inline-flex mb-1 text-sm font-medium" htmlFor="get-token">{t("exchange.to")}</label>
                 <TokenSelect
@@ -54,33 +56,42 @@ function CreateRoom() {
                     ]}
                 />
             </div>
-
+            
             <div className="mt-7">
                 <Checkbox className='hover:cursor-pointer'>
                     <span className="hover:cursor-pointer text-sm">{t("exchange.only_i_can")}</span>
                 </Checkbox>
             </div>
-
+            
             <div className="mt-6">
                 <label className="inline-flex mb-1 text-sm font-medium" htmlFor="">{t("exchange.purchase_limit")}</label>
                 <Input placeholder={t("exchange.it_is_empty")}/>
             </div>
-
-            <div className="mt-12 sm:mt-11">
+            
+            <div className='mt-4'>{localErrorInfoBox}</div>
+            
+            <div className="mt-6 sm:mt-11">
                 <Button
                     disabled={!(from.currency && to.currency)}
                     size="xl"
                     className="w-full"
                     onClick={() => {
                         setLoading(true);
-
+                        
                         apiCreateRoom({
                             currency1: from.currency,
                             currency2: to.currency,
                             flags: 0,
                             to_balance_limit: 0
-                        }).then(response => {
-                            onRoomCreation(response.data.result);
+                        }).then(({data}) => {
+                            if (data.error) {
+                                localErrorHunter({code: 0, message: data.error.message});
+                                return;
+                            }
+                            
+                            onRoomCreation(data.result);
+                        }).catch((e) => {
+                            localErrorHunter(e);
                         }).finally(() => {
                             setLoading(false);
                         });

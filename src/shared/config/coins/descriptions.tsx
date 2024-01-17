42b464af9beaf1c5d5839cf0460458e1bae3ce28
rezucos <tsyganok.vasya@proton.me>
2024-01-17 18:44:28 +0100

    code transfer styles

--- src/shared/config/coins/descriptions.tsx
+++ src/shared/config/coins/descriptions.tsx
@@ -2097,7 +2097,7 @@ export function getTokenDescriptions(navigate: NavigateFunction, account: IWalle
                 </div>
             </div>
 
-            <div className='bg-green rounded-[4px] mb-4 py-5 px-4 text-white border-[#c3e6cb]'>
+            <div className='info-box-description mb-4'>
                 <p className='font-bold text-xl mb-4'>3% {t("AER_interest")}</p>
                 <p>{t("you_get_per_annum")}:</p>
                 <p>(i) {t("your_weighted_average")};</p>

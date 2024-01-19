70259ffc9ad5fd6c5a58568984f07e92db9a1160
aleksmanson <aleksey.muzhichin@gmail.com>
2024-01-19 14:08:08 +0400

    rm workbox rm gate-dev

--- vite.config.ts
+++ vite.config.ts
@@ -24,30 +24,30 @@ export default defineConfig(({mode}) => {
             ],
         },
         server: {
-            // proxy: {
-            // '^/(api|gek|auth|pub/)': {
-            //     target: 'https://dev.gekkard.com',
-            //     changeOrigin: true,
-            //     secure: false,
-            // }
-            // '^/TEMP-API': {
-            //   target: 'http://10.7.14.10/pub/v1/auth',
-            //   changeOrigin: true,
-            //   secure: false,
-            //   rewrite: (path) => path.replace(/^\/TEMP-API/, ''),
-            //   configure: (proxy, _options) => {
-            //     proxy.on('error', (err, _req, _res) => {
-            //       console.log('proxy error', err);
-            //     });
-            //     proxy.on('proxyReq', (proxyReq, req, _res) => {
-            //       console.log(1)
-            //       console.log('Sending Request to the Target:', req.method, req.url);
-            //     });
-            //     proxy.on('proxyRes', (proxyRes, req, _res) => {
-            //       console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-            //     });
-            //   },
-            // },
+            proxy: {
+                '^/(api|gek|pub/)': {
+                    target: 'https://api-dev.gekkoin.com',
+                    changeOrigin: true,
+                    secure: false,
+                },
+                // '^/TEMP-API': {
+                //   target: 'http://10.7.14.10/pub/v1/auth',
+                //   changeOrigin: true,
+                //   secure: false,
+                //   rewrite: (path) => path.replace(/^\/TEMP-API/, ''),
+                //   configure: (proxy, _options) => {
+                //     proxy.on('error', (err, _req, _res) => {
+                //       console.log('proxy error', err);
+                //     });
+                //     proxy.on('proxyReq', (proxyReq, req, _res) => {
+                //       console.log(1)
+                //       console.log('Sending Request to the Target:', req.method, req.url);
+                //     });
+                //     proxy.on('proxyRes', (proxyRes, req, _res) => {
+                //       console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                //     });
+                //   },
+            },
             // }
         },
         plugins: [react(), svgr(),

e6e558c735925ce92123f2e3dea9ef70f4172eef
aleksmanson <aleksey.muzhichin@gmail.com>
2024-01-21 14:46:00 +0400

    maximumFileSizeToCacheInBytes: 20000000

--- vite.config.ts
+++ vite.config.ts
@@ -55,6 +55,7 @@ export default defineConfig(({mode}) => {
                 registerType: 'autoUpdate',
                 includeAssets: ['**/*'],
                 workbox: {
+                    maximumFileSizeToCacheInBytes: 20000000,
                     globPatterns: ['**/*'],
                     // runtimeCaching: [
                     //     {

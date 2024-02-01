6497d2d09733489f1afbe1c19159bb88066a7c45
rezucos <tsyganok.vasya@proton.me>
2024-02-01 16:31:56 +0100

    undo: versions desc generator

--- increment-version.js
+++ increment-version.js
@@ -1,8 +1,6 @@
 import {writeFileSync} from 'fs';
 import path from 'path'; // Add this line
-import {exec} from 'child_process';
 import packageJsonPath from './package.json' assert {type: 'json'};
-import versionsJsonPath from './versions.json' assert {type: 'json'};
 
 
 try {
@@ -20,23 +18,6 @@ try {
        JSON.stringify(packageJsonPath, null, 2));
     
     console.log(`Version incremented to ${packageJsonPath.version}`);
-    
-    // Version description generation
-    exec('git log -1 --pretty=%B', (error, commitMessage) => {
-        if (error) throw error;
-        
-        versionsJsonPath.unshift({
-            version: packageJsonPath.version,
-            date: new Date(Date.now()),
-            description: commitMessage
-        });
-        
-        writeFileSync(
-            path.resolve(process.cwd(), 'versions.json'),
-            JSON.stringify(versionsJsonPath, null, 2));
-
-        console.log(`Version description: ${commitMessage}`);
-    });
 } catch (error) {
     console.error('Error incrementing version:', error);
     process.exit(1);

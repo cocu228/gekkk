9d4d221c8472bd330ff63b92bd8edecf4d44c829
dreamybo1 <idelchiksasha@gmail.com>
2024-02-06 21:39:37 +0500

    added some localization

--- src/widgets/wallet/transfer/top-up/ui/forms/TopUpFormSepa.tsx
+++ src/widgets/wallet/transfer/top-up/ui/forms/TopUpFormSepa.tsx
@@ -1,20 +1,21 @@
 import {useContext} from 'react';
 import {CtxRootData} from "@/processes/RootContext";
 import CopyIcon from "@/shared/ui/copy-icon/CopyIcon";
+import { useTranslation } from 'react-i18next';
 // import {storeOrganizations} from "@/shared/store/organizations";
 
 const TopUpFormSepa = () => {
 
     // const organizations = storeOrganizations(state => state.organizations);
     const {account} = useContext(CtxRootData);
-
+    const {t} = useTranslation()
 
     return (<div className="wrapper">
         <div className="row mb-8 w-full">
             <div className="col">
                 <div className="row mb-2">
                     <div className="col">
-                        <span className="text-gray-400">Account for top up:</span>
+                        <span className="text-gray-400">{t("account_for_top_up")}:</span>
                     </div>
                 </div>
                 <div className="row">
@@ -47,7 +48,7 @@ const TopUpFormSepa = () => {
             <div className="col">
                 <div className="row mb-2">
                     <div className="col">
-                        <span className="text-gray-400">Recipient:</span>
+                        <span className="text-gray-400">{t("recipient")}:</span>
                     </div>
                 </div>
                 <div className="row">
@@ -62,7 +63,7 @@ const TopUpFormSepa = () => {
             <div className="col">
                 <div className="row mb-2">
                     <div className="col">
-                        <span className="text-gray-400">The beneficiary’s bank:</span>
+                        <span className="text-gray-400">{t("the_beneficiary_bank")}:</span>
                     </div>
                 </div>
                 <div className="row">
@@ -77,7 +78,7 @@ const TopUpFormSepa = () => {
             <div className="col">
                 <div className="row mb-2">
                     <div className="col">
-                        <span className="text-gray-400">BIC of beneficiary’s bank:</span>
+                        <span className="text-gray-400">{t("bic_of_beneficiary_bank")}:</span>
                     </div>
                 </div>
                 <div className="row">

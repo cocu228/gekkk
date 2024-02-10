e6f1136de720e4c9bb7e582921948e755c5b2a45
psix <salveret@gmail.com>
2024-02-10 02:20:36 +0100

    styles

--- src/app/authentication/src/pages/auth/index.tsx
+++ src/app/authentication/src/pages/auth/index.tsx
@@ -2,7 +2,6 @@
 import { useBreakpoints } from "../../app/providers/BreakpointsProvider";
 import styles from './style.module.css';
 
-import TabButton from "../../widgets/components/tab-button/TabButton";
 import { useState } from "react";
 import { LoginPasswordForm } from "../../widgets/LoginPasswordForm";
 import { LoginDeviceKey } from "../../widgets/LoginDeviceKey";
@@ -35,54 +34,33 @@ const Auth = () => {
 					emailCode ? <ResetPasswordForm emailCode={emailCode} />
 						:
 						<div className={styles.MainBody}>
-
 							<header>
-								<h1 style={{ color: 'var(--gek-text-primary)', marginBottom: '18px' }}>
+								<h1>
 									Welcome to Gekkard online bank
 								</h1>
-								<p className={styles.FormHeader}>
+								<p>
 									Log in using the form below
 								</p>
 							</header>
 
-							{/* Input form */}
 							<main>
-								<div style={{
-									width: 'auto',
-									margin: '4px 4px 0 4px',
-									gap: '1px',
-									display: 'grid',
-									gridTemplateColumns: 'auto auto'
-								}}>
-									<TabButton active={tab === 'PASSWORD'} onClick={() => setTab('PASSWORD')}>
+								<div className={styles.FormTab}>
+									<button className={`${styles.TabButton} ${tab === 'PASSWORD' ? styles.TabButtonActive : ""}`} onClick={() => setTab('PASSWORD')} >
 										Password
-									</TabButton>
-
-									<TabButton active={tab === 'DEVICE_KEY'} onClick={() => setTab('DEVICE_KEY')}>
+									</button>
+									<button className={`${styles.TabButton} ${tab === 'DEVICE_KEY' ? styles.TabButtonActive : ""}`} onClick={() => setTab('DEVICE_KEY')} >
 										Device key
-									</TabButton>
+									</button>
 								</div>
-
-								<div className={styles.FormSubstrate}>
-									<div className={styles.FormWrapper}>
-										<form className={styles.FormBody}>
-											{tab === 'PASSWORD'
-												? <LoginPasswordForm />
-												: <LoginDeviceKey />
-											}
-										</form>
-									</div>
-								</div>
+								<form className={styles.FormBody}>
+									{tab === 'PASSWORD'
+										? <LoginPasswordForm />
+										: <LoginDeviceKey />
+									}
+								</form>
 							</main>
-
-							{/* Under input form */}
-							<details style={{
-								width: '100%',
-								display: 'flex',
-								justifyContent: 'space-between',
-								alignItems: 'center',
-								paddingTop: '60px'
-							}}>
+							
+							<details>
 								<summary>Don’t have an account?</summary>
 
 								<a href="https://webreg.gekkard.com/" target="_blank" rel="noreferrer noopener">
@@ -90,11 +68,8 @@ const Auth = () => {
 								</a>
 							</details>
 
-							<footer style={{ flex: '0 0 auto', display: 'flex', flexDirection: 'column' }}>
-								<nav className="typography-b2" style={{
-									marginBottom: '6px', color: '285E69ff',
-									display: 'flex', justifyContent: 'space-between'
-								}}>
+							<footer>
+								<nav>
 									<a href="https://gekkard.com/terms-and-conditions.html" target="_blank" rel="noreferrer noopener">
 										General terms and conditions
 									</a>
@@ -108,11 +83,10 @@ const Auth = () => {
 									</a>
 								</nav>
 
-								<div style={{ color: 'var(--gek-dark-grey)', marginBottom: '3px' }}>
+								<p>
 									Crypto exchange service is powered by AtlantEX OU (licensed partner for crypto wallet and exchange)
-								</div>
-
-								<div style={{ color: 'var(--gek-dark-grey)' }}>© Gekkoin. v."2.0.65"</div>
+								</p>
+								<p>© Gekkard. v."2.0.65"</p>
 							</footer>
 						</div>
 				}

3cc8ddfebc5a114b2b31eca76d66b06dc5a89fdf
rezucos <tsyganok.vasya@proton.me>
2024-02-14 16:46:16 +0100

    fix: sidebar rounding

--- src/widgets/sidebar/model/helpers.ts
+++ src/widgets/sidebar/model/helpers.ts
@@ -5,14 +5,9 @@ export const helperFilterList = (list: Array<ICtxCurrency>) =>
         ((($const !== "EURG" && $const !== "GKE" && $const !== "EUR") && availableBalance && !availableBalance.equals(0))) ||
         (($const !== "EURG" && $const !== "GKE" && $const !== "EUR") && (lockInBalance && lockInBalance > 0)));
 
-export const toLocaleCryptoRounding = (value: number, maximumFractionDigits?: number, minimumFractionDigits?: number): string | null => {
-	const newVal = value?.toLocaleString("eu", { maximumFractionDigits, minimumFractionDigits});
-    if (!newVal) return null;
-    return newVal.replace(',', ' ');
-}
+export const toLocaleCryptoRounding = (value: number, maximumFractionDigits?: number, minimumFractionDigits?: number): string | null =>
+	value?.toLocaleString("eu", { maximumFractionDigits, minimumFractionDigits});
 
-export const toLocaleFiatRounding = (value: number): string | null =>{
-	const newVal = value?.toLocaleString("eu", { maximumFractionDigits: 2, minimumFractionDigits: 2});
-    if (!newVal) return null;
-    return newVal.replace(',', ' ');
-};
+export const toLocaleFiatRounding = (value: number): string | null =>
+	value?.toLocaleString("eu", { maximumFractionDigits: 2, minimumFractionDigits: 2});
+

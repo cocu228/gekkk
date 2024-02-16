c5b5e6602a5e425a9650c2d26a3a1958282b2724
dreamybo1 <idelchiksasha@gmail.com>
2024-02-17 00:30:15 +0500

    wallet header mobile short name fix, transfers change type customization select

--- src/widgets/wallet/transfer/mobile/ui/choose-network-mobile/index.tsx
+++ src/widgets/wallet/transfer/mobile/ui/choose-network-mobile/index.tsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import InfoBox from "@/widgets/info-box";
 import {useNavigate, useParams} from "react-router-dom";
 import {CurrencyFlags} from "@/shared/config/mask-currency-flags";
@@ -17,7 +17,9 @@ const ChooseNetworkMobile = ({withdraw = false}) => {
     const navigate = useNavigate();
     const {currencies} = useContext(CtxCurrencies);
     const {currency} = useParams()
-    
+    const [open, setOpen] = useState<boolean>(false)
+
+
     
     
     const {setNetworkType, networksForSelector, networkTypeSelect} = useContext(CtxWalletNetworks);
@@ -25,19 +27,47 @@ const ChooseNetworkMobile = ({withdraw = false}) => {
         && !(Array.isArray(networksForSelector) && networksForSelector.length === 0)
         && currency !== "EURG"
         && (isCryptoNetwork(networkTypeSelect));
-        console.log(networksForSelector);
         
+    const networkList = networksForSelector ? networksForSelector.map(el => {
+        return {
+            value:el.value,
+            label:<div className='w-full items-center flex justify-center color-[#3A5E66]'>
+                {el.label}
+            </div> 
+        }
+    }) : [{
+        value:0,
+        label:"none"
+    }]
     
     return( 
         <>
             <div className="row w-full font-medium">
                 <div className="col">
                     <Select
+                        open={open}
+                        onDropdownVisibleChange={(visible) => setOpen(visible)}
+                        dropdownStyle={{backgroundColor:"transparent", boxShadow:"none"}}
                         className="w-full"
                         placeholder={"Networks not found"} value={networkTypeSelect}
                         onSelect={setNetworkType}
                         options={networksForSelector}
                         listHeight={500}
+                        dropdownRender={(menu)=>{
+                            return(
+                                <div 
+                                    className='h-[380px] overflow-auto'
+                                >
+                                        {networkList.map((el)=>{
+                                            return(
+                                                <div style={{backgroundColor:`${(networkTypeSelect === el.value)?"#7B797C":"white"}`, display:"flex", alignItems:"center", justifyContent:"start", paddingLeft:"5px", color:"#3A5E66"}} onClick={()=>{setNetworkType(el.value);setOpen(false)}}className={`grid grid-cols-[repeat(3,1fr)] grid-rows-[1fr] gap-x-2.5 gap-y-0 mb-[5px] items-center justify-center gap-[5px] rounded-md h-[30px] bg-opacity-1 border-[1px] border-solid border-[#E0E0E0]`}>
+                                                    <span className='self-center text-[12px]'>{el.label}</span>
+                                                </div>
+                                            )
+                                        })}
+                                </div>
+                            )
+                        }}
                     />
                 </div>
             </div>

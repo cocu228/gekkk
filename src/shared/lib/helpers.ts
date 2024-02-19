8f626a13d8e9fad4c83e5c13fbd9c6142cef7a3d
rezucos <tsyganok.vasya@proton.me>
2024-02-19 11:13:15 +0100

    phone number in cookies

--- src/shared/lib/helpers.ts
+++ src/shared/lib/helpers.ts
@@ -244,6 +244,7 @@ export function clearCookie(name: string) {
 export const logout = async () => {
     await apiLogout();
     clearCookie("accountId");
+    clearCookie("phoneNumber");
     location.replace('/');
 };
 

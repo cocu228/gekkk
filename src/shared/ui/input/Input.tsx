9f405644266c2f1335a5a13b87b3e4bf5289616c
Anton <you@example.com>
2024-04-03 22:35:58 +0300

    input selector changed

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./style.module.scss";
-import {validateInput} from "./model/helpers";
-import {Input as InputAntd, InputProps, InputRef} from "antd";
+import { validateInput } from "./model/helpers";
+import { Input as InputAntd, InputProps, InputRef } from "antd";
 
 type IParams = InputProps & {
   allowDigits?: boolean;
@@ -9,31 +9,36 @@ type IParams = InputProps & {
   wrapperClassName?: string;
 };
 
-const Input = React.forwardRef(({
-  onChange,
-  onKeyDown,
-  allowDigits,
-  allowSymbols,
-  wrapperClassName,
-  ...props
-}: IParams, ref: React.ForwardedRef<InputRef>) => {
-  return (
-    <div
-      className={`${styles.Input} ${
-        wrapperClassName ? wrapperClassName : ""
-      }`}
-    >
-      <InputAntd
-        {...props}
-        ref={ref}
-        onChange={(event) => {
-          if (validateInput(event, allowDigits, allowSymbols)) {
-            onChange(event);
-          }
-        }}
-      />
-    </div>
-  );
-});
+const Input = React.forwardRef(
+  (
+    {
+      onChange,
+      onKeyDown,
+      allowDigits,
+      allowSymbols,
+      wrapperClassName,
+      ...props
+    }: IParams,
+    ref: React.ForwardedRef<InputRef>
+  ) => {
+    return (
+      <div
+        className={`${styles.Input} ${
+          wrapperClassName ? wrapperClassName : ""
+        }`}
+      >
+        <InputAntd
+          {...props}
+          ref={ref}
+          onChange={(event) => {
+            if (validateInput(event, allowDigits, allowSymbols)) {
+              onChange(event);
+            }
+          }}
+        />
+      </div>
+    );
+  }
+);
 
 export default Input;
---
+++ src/widgets/exchange/components/selectToken/SelectToken.tsx
@@ -0,0 +1,115 @@
+import s from "./styles.module.scss";
+import { FC, useContext, useState } from "react";
+import CurrencySelector from "@/shared/ui/input-currency/ui/currency-selector/CurrencySelector";
+import { CtxExchangeData } from "@/widgets/exchange/model/context";
+import { IconCoin } from "@/shared/ui/icons/icon-coin";
+import Input from "@/shared/ui/input/Input";
+import DownArr from "@/assets/downArr.svg?react";
+import InputCurrency from "@/shared/ui/input-currency/ui";
+import { CtxCurrencies } from "@/processes/CurrenciesContext";
+import { useTranslation } from "react-i18next";
+import {
+  validateBalance,
+  validateMinimumAmount,
+} from "@/shared/config/validators";
+import { useNavigate } from "react-router-dom";
+import Decimal from "decimal.js";
+
+interface SelectTokenProps {
+  roomType: string;
+  excludedCurrencies: any;
+  allowedFlags: any;
+  onSelect: any;
+  currency: any;
+  valueChange?: any;
+  isValidator?: boolean;
+}
+
+export const SelectToken: FC<SelectTokenProps> = ({
+  roomType,
+  excludedCurrencies,
+  allowedFlags,
+  onSelect,
+  currency,
+  isValidator,
+  valueChange,
+}) => {
+  const {
+    to,
+    from,
+    price,
+    roomInfo,
+    isLimitOrder,
+    onToValueChange,
+    onCurrenciesSwap,
+    onFromValueChange,
+    onToCurrencyChange,
+    onFromCurrencyChange,
+    onIsLimitOrderChange,
+  } = useContext(CtxExchangeData);
+
+  const { currencies } = useContext(CtxCurrencies);
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const minAmount = currencies.get(from.currency)
+    ? new Decimal(currencies.get(from.currency)?.minOrder).toNumber()
+    : 0;
+
+  return (
+    <>
+      <div className={s.select_wrap}>
+        <div className={s.selected_body}>
+          <CurrencySelector
+            balanceFilter
+            onSelect={onSelect}
+            disabled={false}
+            excludedCurrencies={excludedCurrencies}
+            allowedFlags={allowedFlags}
+          >
+            {!currency ? (
+              <span className={s.select_preTitle}>{t("select_token")}</span>
+            ) : (
+              <span className={s.selected_token}>
+                <IconCoin width={34} height={34} code={currency} />
+                {currency}
+              </span>
+            )}
+          </CurrencySelector>
+        </div>
+        <DownArr className="min-w-[7px] h-[12px]" />
+        <div className={s.input_body}>
+          <input
+            onChange={(event) => {
+              const value: string = event.target.value;
+              valueChange(value);
+            }}
+            className={s.input}
+            type="number"
+            placeholder="-enter amount-"
+          />
+        </div>
+      </div>
+      {isValidator && (
+        <InputCurrency.Validator
+          className="text-sm"
+          value={+from.amount}
+          description={
+            !from.currency
+              ? null
+              : t("minimum_order_amount", {
+                  amount:
+                    currencies.get(from.currency)?.minOrder +
+                    " " +
+                    from.currency,
+                })
+          }
+          validators={[
+            validateBalance(currencies.get(from.currency), navigate, t),
+            validateMinimumAmount(minAmount, +from.amount, from.currency, t),
+          ]}
+        ></InputCurrency.Validator>
+      )}
+    </>
+  );
+};

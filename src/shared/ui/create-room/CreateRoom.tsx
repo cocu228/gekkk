7836a1bf955daa5c80dcb463133610ded1708c18
Anton <you@example.com>
2024-04-04 16:02:53 +0300

    logic ++, desktop exchange create room modal fixed

--- src/shared/ui/create-room/CreateRoom.tsx
+++ src/shared/ui/create-room/CreateRoom.tsx
@@ -11,7 +11,11 @@ import { IExchangeField } from "@/widgets/exchange/model/types";
 import { CurrencyFlags } from "@/shared/config/mask-currency-flags";
 import ModalInfoText from "@/shared/ui/modal/modal-info-text/ModalInfoText";
 import TokenSelect from "@/shared/ui/search-select/token-select/TokenSelect";
-
+import IconSwap from "@/shared/ui/icons/IconSwap";
+import s from "./styles.module.scss";
+import WarningIcon from "@/assets/MobileModalWarningIcon.svg?react";
+import { Switch } from "antd";
+import { is } from "date-fns/locale";
 interface IState {
   isIco: boolean;
   purchaseLimit: number;
@@ -34,23 +38,25 @@ function CreateRoom({
 }: IParams) {
   const [loading, setLoading] = useState<boolean>(false);
   const [localErrorHunter, , localErrorInfoBox] = UseError();
-  const [state, setState] = useState<IState>({
-    isIco: false,
-    purchaseLimit: 0,
-  });
+
+  const [purchaseLimit, setPurchaseLimit] = useState(0);
+  const [isIco, setIsIco] = useState(false);
 
   const purchaseLimitChange = (target: any) => {
-    setState({
-      ...state,
-      purchaseLimit: +target.value,
-    });
+    setPurchaseLimit(+target.value);
   };
+
+  console.log(isIco);
 
   return (
     <>
       <div className={loading ? "!collapse" : ""}>
         <ModalInfoText>{t("exchange.private_room_allows")}</ModalInfoText>
 
+        <div className={s.modal_text}>
+          <WarningIcon />
+          {t("exchange.private_room_allows")}
+        </div>
         <div className="mt-4 mb-10">
           <label
             className="inline-flex mb-1 text-sm font-medium"
@@ -67,7 +73,9 @@ function CreateRoom({
             allowedFlags={[CurrencyFlags.ExchangeAvailable]}
           />
         </div>
-
+        <div className="w-[25px] h-[25px] m-[10px_auto_0_auto]">
+          <IconSwap />
+        </div>
         <div className="mt-2">
           <label
             className="inline-flex mb-1 text-sm font-medium"
@@ -84,39 +92,28 @@ function CreateRoom({
             allowedFlags={[CurrencyFlags.ExchangeAvailable]}
           />
         </div>
-
-        <div className="mt-7">
-          <Checkbox
-            defaultChecked={state.isIco}
-            className="hover:cursor-pointer"
-            onChange={(val) =>
-              setState({
-                ...state,
-                isIco: val,
-              })
-            }
+        <div className="flex items-center gap-3 justify-center mt-[15px]">
+          <div
+            onClick={() => setIsIco(!isIco)}
+            className={`w-[40px] cursor-pointer h-[19px] rounded-[40px] transition-all duration-300 ${
+              isIco ? "bg-[#b4c0cd]" : "bg-[#2BAB72]"
+            } relative p-[4px] `}
           >
-            <span className="hover:cursor-pointer text-sm">
-              {t("exchange.only_i_can")}
-            </span>
-          </Checkbox>
-        </div>
-
-        <div className="mt-6">
-          <label className="inline-flex mb-1 text-sm font-medium" htmlFor="">
-            {t("exchange.purchase_limit")}
-          </label>
-          <Input
-            allowDigits
-            placeholder={t("exchange.it_is_empty")}
-            // onChange={({ target }) =>
-            //   setState({
-            //     ...state,
-            //     purchaseLimit: +target.value,
-            //   })
-            // }
-            onChange={purchaseLimitChange}
-          />
+            <div
+              className={`w-[15px] h-[15px] transition-all duration-300 rounded-[50%] absolute  top-[2.2px] 
+                ${isIco ? "left-[2px]" : "left-[calc(100%_-_17.5px)]"}
+                ${isIco ? "bg-[#fff]" : "bg-[#285E69]"}
+              `}
+            ></div>
+          </div>
+          <span
+            onClick={() => {
+              setIsIco(!isIco);
+            }}
+            className="hover:cursor-pointer text-smm select-none"
+          >
+            {t("exchange.only_i_can")}
+          </span>
         </div>
 
         <div className="mt-4">{localErrorInfoBox}</div>
@@ -132,8 +129,8 @@ function CreateRoom({
               apiCreateRoom({
                 currency1: from.currency,
                 currency2: to.currency,
-                flags: state.isIco ? 1 : 0,
-                to_balance_limit: state.purchaseLimit,
+                flags: isIco ? 1 : 0,
+                to_balance_limit: purchaseLimit,
               })
                 .then(({ data }) => {
                   if (data.error) {
--- src/widgets/header/ui/mobile/ExchangeRoomMenu.tsx
+++ src/widgets/header/ui/mobile/ExchangeRoomMenu.tsx
@@ -1,222 +1,259 @@
-import styles from './style.module.scss';
-import {useEffect, useState} from "react";
-import {useTranslation} from 'react-i18next';
+import styles from "./style.module.scss";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import Dropdown from "@/shared/ui/dropdown/Dropdown";
 import DropdownItem from "@/shared/ui/dropdown/dropdown-item/DropdownItem";
-import {storeListExchangeRooms} from "@/shared/store/exchange-rooms/exchangeRooms";
+import { storeListExchangeRooms } from "@/shared/store/exchange-rooms/exchangeRooms";
 import IconPrivateRoom from "@/shared/ui/icons/IconPrivateRoom";
-import IconParticipant from '@/shared/ui/icons/IconParticipant';
-import {useNavigate} from 'react-router-dom';
-import {RoomInfo} from '@/shared/(orval)api/gek/model';
-import Modal from '@/shared/ui/modal/Modal';
-import InviteLink from '@/shared/ui/invite-link/InviteLink';
-import Button from '@/shared/ui/button/Button';
-import {apiCloseRoom} from '@/shared/(orval)api';
-import useModal from '@/shared/model/hooks/useModal';
-import IconClose from '@/shared/ui/icons/IconClose';
-import IconQR from '@/shared/ui/icons/IconQR';
-import IconAddRoom from '@/shared/ui/icons/IconAddRoom';
-import CreateRoom from '@/shared/ui/create-room/CreateRoom';
-import { IExchangeField } from '@/widgets/exchange/model/types';
-import LogoutMobileIcon from "@public/img/icon/LogoutMobileIcon.svg"
+import IconParticipant from "@/shared/ui/icons/IconParticipant";
+import { useNavigate } from "react-router-dom";
+import { RoomInfo } from "@/shared/(orval)api/gek/model";
+import Modal from "@/shared/ui/modal/Modal";
+import InviteLink from "@/shared/ui/invite-link/InviteLink";
+import Button from "@/shared/ui/button/Button";
+import { apiCloseRoom } from "@/shared/(orval)api";
+import useModal from "@/shared/model/hooks/useModal";
+import IconClose from "@/shared/ui/icons/IconClose";
+import IconQR from "@/shared/ui/icons/IconQR";
+import IconAddRoom from "@/shared/ui/icons/IconAddRoom";
+import CreateRoom from "@/shared/ui/create-room/CreateRoom";
+import { IExchangeField } from "@/widgets/exchange/model/types";
+import LogoutMobileIcon from "@public/img/icon/LogoutMobileIcon.svg";
 
-export const ExchangeRoomMenu = ({
-    roomId
-}: {
-    roomId: string;
-}) => {
-    const {t} = useTranslation();
-    const navigate = useNavigate();
-    const roomModal = useModal();
-    const roomCloseModal = useModal();
-    const [to, setTo] = useState<IExchangeField>({
-        amount: null,
-        currency: null
-    });
-    const [from, setFrom] = useState<IExchangeField>({
-        amount: null,
-        currency: null
-    });
-    const [active, setActive] = useState<RoomInfo>(null);
-    const {
-        roomsList,
-        removeRoom,
-        addRoom: addExchangeRoom
-    } = storeListExchangeRooms(state => state);
+export const ExchangeRoomMenu = ({ roomId }: { roomId: string }) => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const roomModal = useModal();
+  const roomCloseModal = useModal();
+  const [to, setTo] = useState<IExchangeField>({
+    amount: null,
+    currency: null,
+  });
+  const [from, setFrom] = useState<IExchangeField>({
+    amount: null,
+    currency: null,
+  });
+  const [active, setActive] = useState<RoomInfo>(null);
+  const {
+    roomsList,
+    removeRoom,
+    addRoom: addExchangeRoom,
+  } = storeListExchangeRooms((state) => state);
 
-    useEffect(() => {
-        if (roomsList) {
-            setActive(roomsList.find(r => r.timetick === +roomId));
+  useEffect(() => {
+    if (roomsList) {
+      setActive(roomsList.find((r) => r.timetick === +roomId));
+    }
+  }, [roomsList, roomId]);
+
+  return !roomsList ? null : (
+    <div>
+      <Dropdown
+        className={"min-w-[214px] flex justify-end bg-transparent"}
+        trigger={
+          <div className="flex gap-2 items-center">
+            {!active ? (
+              <span className={styles.HeaderTitle}>{t("exchange.rooms")}</span>
+            ) : (
+              <div className={styles.RoomsMenuItem}>
+                <span className={`${styles.RoomsMenuItemTokens} text-white`}>
+                  {active.currency1} - {active.currency2}
+                </span>
+                <span className={`${styles.RoomsMenuItemNumber} text-white`}>
+                  {active.timetick}
+                </span>
+              </div>
+            )}
+            <button className={styles.ArrowBtn}></button>
+          </div>
         }
-    }, [roomsList, roomId])
+        items={[
+          ...roomsList.map((r) => ({
+            key: r.timetick,
+            label: (
+              <RoomItem room={r} isActive={r.timetick === active?.timetick} />
+            ),
+          })),
+          ...(!active
+            ? [
+                {
+                  key: "new-room",
+                  label: (
+                    <DropdownItem
+                      className="w-full bg-[#DCDCD9]"
+                      onClick={roomModal.showModal}
+                    >
+                      <div className="flex justify-between items-center w-full">
+                        <span className="font-semibold text-[#1F3446]">
+                          {t("exchange.new_room")}
+                        </span>
 
-    return !roomsList ? null : (
-        <div>
-            <Dropdown
-                className={'min-w-[214px] flex justify-end bg-transparent'}
-                trigger={<div className='flex gap-2 items-center'>
-                    {!active ? <span className={styles.HeaderTitle}>{t("exchange.rooms")}</span> : (
-                        <div className={styles.RoomsMenuItem}>
-                            <span className={`${styles.RoomsMenuItemTokens} text-white`}>
-                                {active.currency1} - {active.currency2}
-                            </span>
-                            <span className={`${styles.RoomsMenuItemNumber} text-white`}>
-                                {active.timetick}
-                            </span>
-                        </div>
-                    )}
-                    <button className={styles.ArrowBtn}></button>
-                </div>}
-                items={[
-                    ...roomsList.map(r => ({
-                        key: r.timetick,
-                        label: <RoomItem room={r} isActive={r.timetick === active?.timetick}/>
-                    })),
-                    ...(!active ? [{
-                        key: 'new-room',
-                        label: <DropdownItem className='w-full bg-[#DCDCD9]' onClick={roomModal.showModal}>
-                            <div className='flex justify-between items-center w-full'>
-                                <span className='font-semibold text-[#1F3446]'>{t("exchange.new_room")}</span>
+                        <IconAddRoom size={26} stroke="#285E69" />
+                      </div>
+                    </DropdownItem>
+                  ),
+                },
+              ]
+            : [
+                {
+                  key: "invite-link",
+                  label: (
+                    <DropdownItem
+                      className="w-full bg-[#DCDCD9]"
+                      onClick={roomModal.showModal}
+                    >
+                      <div className="flex justify-between items-center w-full">
+                        <span className="font-semibold text-[#1F3446]">
+                          {t("invite_link")}
+                        </span>
 
-                                <IconAddRoom
-                                    size={26}
-                                    stroke='#285E69'
-                                />
-                            </div>
-                        </DropdownItem>
-                    }] : [{
-                        key: 'invite-link',
-                        label: <DropdownItem className='w-full bg-[#DCDCD9]' onClick={roomModal.showModal}>
-                            <div className='flex justify-between items-center w-full'>
-                                <span className='font-semibold text-[#1F3446]'>{t("invite_link")}</span>
+                        <IconQR size={26} stroke="#285E69" />
+                      </div>
+                    </DropdownItem>
+                  ),
+                },
+                {
+                  key: "close-room",
+                  label: (
+                    <DropdownItem
+                      className="w-full border-b-1 border-[#285e69] bg-[#DCDCD9]"
+                      onClick={roomCloseModal.showModal}
+                    >
+                      <div className="flex justify-between items-center w-full">
+                        <span className="font-semibold text-[#8F123A]">
+                          {t("close_current_room")}
+                        </span>
 
-                                <IconQR
-                                    size={26}
-                                    stroke='#285E69'
-                                />
-                            </div>
-                        </DropdownItem>
-                    }, {
-                        key: 'close-room',
-                        label: <DropdownItem className='w-full border-b-1 border-[#285e69] bg-[#DCDCD9]' onClick={roomCloseModal.showModal}>
-                            <div className='flex justify-between items-center w-full'>
-                                <span className='font-semibold text-[#8F123A]'>{t("close_current_room")}</span>
+                        <IconClose size={22} stroke="#8F123A" />
+                      </div>
+                    </DropdownItem>
+                  ),
+                },
+                {
+                  key: "back",
+                  label: (
+                    <DropdownItem
+                      className="w-full bg-[#DCDCD9]"
+                      onClick={() => navigate("/exchange")}
+                    >
+                      <div className="flex justify-between items-center w-full">
+                        <span className="font-semibold text-[#1F3446]">
+                          {t("back_to_exchange")}
+                        </span>
 
-                                <IconClose
-                                    size={22}
-                                    stroke='#8F123A'
-                                />
-                            </div>
-                        </DropdownItem>
-                    }, {
-                        key: 'back',
-                        label: <DropdownItem className='w-full bg-[#DCDCD9]' onClick={() => navigate('/exchange')}>
-                            <div className='flex justify-between items-center w-full'>
-                                <span className='font-semibold text-[#1F3446]'>{t("back_to_exchange")}</span>
+                        <img src={LogoutMobileIcon} height={22} width={22} />
+                      </div>
+                    </DropdownItem>
+                  ),
+                },
+              ]),
+        ]}
+      />
 
-                                <img
-                                    src={LogoutMobileIcon}
-                                    height={22}
-                                    width={22}
-                                />
-                            </div>
-                        </DropdownItem>
-                    }])
-                ]}
-            />
+      <Modal
+        className={styles.RoomModal}
+        width={450}
+        open={roomModal.isModalOpen}
+        onCancel={roomModal.handleCancel}
+        title={
+          active ? t("invite_link") : t("exchange.open_private_exchange_room")
+        }
+      >
+        {active ? (
+          <InviteLink roomInfo={active} />
+        ) : (
+          <CreateRoom
+            to={to}
+            from={from}
+            onRoomCreation={(roomInfo) => {
+              addExchangeRoom(roomInfo);
+              roomModal.handleCancel();
+              navigate(`/private-room?roomId=${roomInfo.timetick}`);
+            }}
+            onToCurrencyChange={(value) => setTo({ ...to, currency: value })}
+            onFromCurrencyChange={(value) =>
+              setFrom({ ...from, currency: value })
+            }
+          />
+        )}
+      </Modal>
 
-            <Modal
-                width={450}
-                open={roomModal.isModalOpen}
-                onCancel={roomModal.handleCancel}
-                title={active ? t("invite_link") : t("exchange.open_private_exchange_room")}
-            >
-                {active
-                    ? <InviteLink roomInfo={active}/>
-                    : <CreateRoom
-                        to={to}
-                        from={from}
-                        onRoomCreation={(roomInfo) => {
-                            addExchangeRoom(roomInfo);
-                            roomModal.handleCancel();
-                            navigate(`/private-room?roomId=${roomInfo.timetick}`)
-                        }}
-                        onToCurrencyChange={(value) => setTo({...to, currency: value})}
-                        onFromCurrencyChange={(value) => setFrom({...from, currency: value})}
-                    />
-                }
-            </Modal>
-
-            <Modal
-                width={450}
-                open={roomCloseModal.isModalOpen}
-                onCancel={roomCloseModal.handleCancel}
-                title={t("invite_link")}
-            >
-                <div className="pt-5 text-sm">
-                    {active ?
-                            t("are_you_sure_close", {currency1: active?.currency1, currency2: active?.currency2})
-                        : 
-                            t("are_you_sure_leave", {currency1: active?.currency1, currency2: active?.currency2})
-                    }
-                </div>
-                
-                <div className="mt-16 sm:mt-14">
-                    <Button
-                        size="xl"
-                        className="w-full"
-                        onClick={() => {
-                            apiCloseRoom({
-                                roomId: active.timetick
-                            }).then(() => {
-                                removeRoom(active.timetick);
-                                roomCloseModal.handleCancel();
-                                navigate('/exchange');
-                            }).catch(roomCloseModal.handleCancel);
-                        }}
-                    >{t("close_private_exchange_room")}</Button>
-                </div>
-            </Modal>
+      <Modal
+        width={450}
+        open={roomCloseModal.isModalOpen}
+        onCancel={roomCloseModal.handleCancel}
+        title={t("invite_link")}
+      >
+        <div className="pt-5 text-sm">
+          {active
+            ? t("are_you_sure_close", {
+                currency1: active?.currency1,
+                currency2: active?.currency2,
+              })
+            : t("are_you_sure_leave", {
+                currency1: active?.currency1,
+                currency2: active?.currency2,
+              })}
         </div>
-    )
-}
+
+        <div className="mt-16 sm:mt-14">
+          <Button
+            size="xl"
+            className="w-full"
+            onClick={() => {
+              apiCloseRoom({
+                roomId: active.timetick,
+              })
+                .then(() => {
+                  removeRoom(active.timetick);
+                  roomCloseModal.handleCancel();
+                  navigate("/exchange");
+                })
+                .catch(roomCloseModal.handleCancel);
+            }}
+          >
+            {t("close_private_exchange_room")}
+          </Button>
+        </div>
+      </Modal>
+    </div>
+  );
+};
 
 const RoomItem = ({
-    room,
-    isActive
+  room,
+  isActive,
 }: {
-    room: RoomInfo;
-    isActive: boolean;
+  room: RoomInfo;
+  isActive: boolean;
 }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <DropdownItem
-            className={`w-full py-2.5 px-2.5 ${isActive ? 'bg-[#DCDCD9]' : ''}`}
-            icon={<div className={styles.RoomsMenuItemIcon}>
-                <IconPrivateRoom/>
-            </div>}
-            onClick={() => navigate(`private-room?roomId=${room.timetick}`)}
+  return (
+    <DropdownItem
+      className={`w-full py-2.5 px-2.5 ${isActive ? "bg-[#DCDCD9]" : ""}`}
+      icon={
+        <div className={styles.RoomsMenuItemIcon}>
+          <IconPrivateRoom />
+        </div>
+      }
+      onClick={() => navigate(`private-room?roomId=${room.timetick}`)}
+    >
+      <div className="flex flex-auto justify-between w-full">
+        <div className={styles.RoomsMenuItem}>
+          <span className={styles.RoomsMenuItemTokens}>
+            {room.currency1} - {room.currency2}
+          </span>
+          <span className={styles.RoomsMenuItemNumber}>{room.timetick}</span>
+        </div>
+
+        <div
+          className={`${styles.RoomsMenuItemIcon} flex items-center gap-[1px]`}
         >
-            <div className='flex flex-auto justify-between w-full'>
-                <div className={styles.RoomsMenuItem}>
-                    <span className={styles.RoomsMenuItemTokens}>
-                        {room.currency1} - {room.currency2}
-                    </span>
-                    <span className={styles.RoomsMenuItemNumber}>
-                        {room.timetick}
-                    </span>
-                </div>
-                
-                <div className={`${styles.RoomsMenuItemIcon} flex items-center gap-[1px]`}>
-                    <span>{room.count}</span>
-                    <IconParticipant
-                        height={24}
-                        width={24}
-                        fill='inherit'
-                    />
-                </div>
-            </div>
-        </DropdownItem>
-    );
-}
+          <span>{room.count}</span>
+          <IconParticipant height={24} width={24} fill="inherit" />
+        </div>
+      </div>
+    </DropdownItem>
+  );
+};

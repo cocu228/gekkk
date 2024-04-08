88a8a2bd9853e5bc9df47540b17a268032099f66
dreamybo1 <idelchiksasha@gmail.com>
2024-04-08 17:58:03 +0500

    faq fix

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -25,7 +25,7 @@ export function Faq() {
 
   const scrollHandler = (elementId: string) => {
     const element = document.getElementById(elementId)
-    element.scrollIntoView({ block: "center", behavior: "smooth" })
+    element.scrollIntoView({ block: "start", behavior: "smooth" })
   }
 
   useEffect(() => {

96762299b1998e52672c016e8bd4b2d8f9620114
rezucos <tsyganok.vasya@proton.me>
2024-04-10 00:50:50 +0200

    styles fixes

--- src/shared/ui/ButtonsMobile/Exchange/index.tsx
+++ src/shared/ui/ButtonsMobile/Exchange/index.tsx
@@ -1,33 +1,28 @@
-import { NavLink, useMatch } from "react-router-dom"
-import styles from "../styles.module.scss"
-import { memo, useContext } from "react"
-import { CtxCurrencies } from "@/processes/CurrenciesContext"
-import { CtxWalletData } from "@/widgets/wallet/transfer/model/context"
-import { useTranslation } from "react-i18next"
+import { memo } from "react";
+import styles from "../styles.module.scss";
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 interface IParams {
-    wallet?: boolean
+    to?: string;
+    className?: string;
 }
 
-const ExchangeButton = memo(({wallet}:IParams) => {
+const ExchangeButton = memo(({to, className}:IParams) => {
     const {t} = useTranslation();
-    const exchangePage = useMatch("/exchange");
-    const privateRoom = useMatch("/private-room");
-    const {currencies} = useContext(CtxCurrencies);
-
-    let $constant;
-
-    if(wallet && currencies){
-        const {$const} = useContext(CtxWalletData);
-        $constant = currencies.get($const);
-    }
-
+    
     return(
         <>
-            <NavLink to={$constant?.$const ?`/exchange?from=${$constant?.$const}`:"/exchange"}>
-                <div className={styles.BottomMenuMobileButtons +` ${(exchangePage || privateRoom || wallet) && styles.BottomMenuButtonsActive}`}>
-                    <svg className={styles.BottomMenuMobileButtonsIcons} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                        <path d="M1.66669 8.33333C1.66669 8.33333 1.76779 7.62563 4.69672 4.6967C7.62565 1.76777 12.3744 1.76777 15.3033 4.6967C16.341 5.73443 17.0111 7.0006 17.3135 8.33333M1.66669 8.33333V3.33333M1.66669 8.33333H6.66669M18.3334 11.6667C18.3334 11.6667 18.2323 12.3744 15.3033 15.3033C12.3744 18.2322 7.62565 18.2322 4.69672 15.3033C3.65899 14.2656 2.98893 12.9994 2.68654 11.6667M18.3334 11.6667V16.6667M18.3334 11.6667H13.3334" stroke={(!!exchangePage || !!privateRoom || wallet) ? "#29354C" : "#9D9D9D"} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <NavLink to={to}>
+                <div className={`${styles.MobileButton} ${className}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                        <path
+                            d="M1.66669 8.33333C1.66669 8.33333 1.76779 7.62563 4.69672 4.6967C7.62565 1.76777 12.3744 1.76777 15.3033 4.6967C16.341 5.73443 17.0111 7.0006 17.3135 8.33333M1.66669 8.33333V3.33333M1.66669 8.33333H6.66669M18.3334 11.6667C18.3334 11.6667 18.2323 12.3744 15.3033 15.3033C12.3744 18.2322 7.62565 18.2322 4.69672 15.3033C3.65899 14.2656 2.98893 12.9994 2.68654 11.6667M18.3334 11.6667V16.6667M18.3334 11.6667H13.3334"
+                            stroke="inherit"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
                     </svg>
                     <span>{t("exchange_button")}</span>
                 </div>
@@ -36,4 +31,4 @@ const ExchangeButton = memo(({wallet}:IParams) => {
     )
 })
 
-export default ExchangeButton
+export default ExchangeButton;
--- src/shared/ui/ButtonsMobile/Funds/index.tsx
+++ src/shared/ui/ButtonsMobile/Funds/index.tsx
@@ -1,25 +1,22 @@
-import { NavLink, useMatch } from "react-router-dom"
-import styles from "../styles.module.scss"
-import { memo } from "react"
-import { useTranslation } from "react-i18next"
+import { memo } from "react";
+import styles from "../styles.module.scss";
+import { useTranslation } from "react-i18next";
+import { NavLink, useMatch } from "react-router-dom";
 
 interface IParams {
-    wallet?: boolean
+    to?: string;
+    className?: string;
 }
 
-const FundsButton = memo(({ wallet }: IParams) => {
-    const {t} = useTranslation()
-    const homePage = useMatch("/")
-    const transfersPage = useMatch("/transfers") //not used
-    const exchangePage = useMatch("/exchange")
-    const historyPage = useMatch("/history") //not used   
-    const isOnMainPages = !!homePage || !!transfersPage || !!exchangePage || !!historyPage
+const FundsButton = memo(({ to, className }: IParams) => {
+    const {t} = useTranslation();
 
     return (
         <>
-            <NavLink to={"/"}>
-                <div className={styles.BottomMenuMobileButtons + ` ${(homePage || wallet) && styles.BottomMenuButtonsActive}`}>
-                    <svg width="24" height="22" viewBox="0 0 21 20" fill="none" stroke={(!!homePage || wallet) ? "#29354C":"#B9B9B5"} xmlns="http://www.w3.org/2000/svg">
+            <NavLink to={to}>
+                <div className={`${styles.MobileButton} ${className}`}>
+                    <svg width="24" height="22" viewBox="0 0 21 20" fill="none"
+                        stroke='inherit' xmlns="http://www.w3.org/2000/svg">
                         <path d="M3.25 12.5V8.37289C3.25 7.75473 3.25 7.44566 3.34625 7.17486C3.43135 6.93546 3.57005 6.71868 3.75175 6.5411C3.95729 6.34022 4.23792 6.2107 4.79918 5.95166M3.25 9.33333V14.8333C3.25 15.7668 3.25 16.2335 3.43166 16.59C3.59144 16.9036 3.84641 17.1586 4.16002 17.3183C4.51654 17.5 4.98325 17.5 5.91667 17.5H15.5833C16.5168 17.5 16.9835 17.5 17.34 17.3183C17.6536 17.1586 17.9086 16.9036 18.0683 16.59C18.25 16.2335 18.25 15.7668 18.25 14.8333V9.33333C18.25 8.39991 18.25 7.9332 18.0683 7.57668C17.9086 7.26308 17.6536 7.00811 17.34 6.84832C16.9835 6.66667 16.5168 6.66667 15.5833 6.66667H5.91667C4.98325 6.66667 4.51654 6.66667 4.16002 6.84832C3.84641 7.00811 3.59144 7.26308 3.43166 7.57668C3.25 7.9332 3.25 8.39991 3.25 9.33333Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         <path d="M14.0833 6.66677V3.75065C14.0833 3.05753 14.0833 2.71097 13.9373 2.498C13.8098 2.31192 13.6122 2.18551 13.3898 2.14767C13.1352 2.10435 12.8206 2.24958 12.1913 2.54003L4.79918 5.95176C4.23792 6.2108 3.95729 6.34032 3.75175 6.5412C3.57005 6.71878 3.43135 6.93556 3.34625 7.17496C3.25 7.44576 3.25 7.75483 3.25 8.37299V12.5001M14.5 12.0834H14.5083M3.25 9.33343V14.8334C3.25 15.7669 3.25 16.2336 3.43166 16.5901C3.59144 16.9037 3.84641 17.1587 4.16002 17.3184C4.51654 17.5001 4.98325 17.5001 5.91667 17.5001H15.5833C16.5168 17.5001 16.9835 17.5001 17.34 17.3184C17.6536 17.1587 17.9086 16.9037 18.0683 16.5901C18.25 16.2336 18.25 15.7669 18.25 14.8334V9.33343C18.25 8.40001 18.25 7.9333 18.0683 7.57678C17.9086 7.26318 17.6536 7.00821 17.34 6.84842C16.9835 6.66677 16.5168 6.66677 15.5833 6.66677H5.91667C4.98325 6.66677 4.51654 6.66677 4.16002 6.84842C3.84641 7.00821 3.59144 7.26318 3.43166 7.57678C3.25 7.9333 3.25 8.40001 3.25 9.33343ZM14.9167 12.0834C14.9167 12.3136 14.7301 12.5001 14.5 12.5001C14.2699 12.5001 14.0833 12.3136 14.0833 12.0834C14.0833 11.8533 14.2699 11.6668 14.5 11.6668C14.7301 11.6668 14.9167 11.8533 14.9167 12.0834Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         <path d="M14.5 12.0834H14.5084M14.9167 12.0834C14.9167 12.3135 14.7302 12.5001 14.5 12.5001C14.2699 12.5001 14.0834 12.3135 14.0834 12.0834C14.0834 11.8533 14.2699 11.6667 14.5 11.6667C14.7302 11.6667 14.9167 11.8533 14.9167 12.0834Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -31,4 +28,4 @@ const FundsButton = memo(({ wallet }: IParams) => {
     )
 })
 
-export default FundsButton
+export default FundsButton;

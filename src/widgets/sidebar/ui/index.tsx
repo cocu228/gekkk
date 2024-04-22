56f4b097f6fd4cd4176ebafdcbac951f57e35a41
rezucos <tsyganok.vasya@proton.me>
2024-04-22 16:44:45 +0200

    fix

--- src/widgets/sidebar/ui/index.tsx
+++ src/widgets/sidebar/ui/index.tsx
@@ -1,15 +1,14 @@
 import {useContext} from "react";
-// import SidebarMobile from "./mobile";
-// import SidebarDesktop from "./desktop";
+import SidebarMobile from "./mobile";
+import SidebarDesktop from "./desktop";
 import {BreakpointsContext} from "@/app/providers/BreakpointsProvider";
 
 const Sidebar = () => {
     const {md} = useContext(BreakpointsContext);
 
-    return null
-    // return md
-    //     ? <SidebarMobile/>
-    //     : <SidebarDesktop/>;
+    return md
+        ? <SidebarMobile/>
+        : <SidebarDesktop/>;
 }
 
 export default Sidebar;

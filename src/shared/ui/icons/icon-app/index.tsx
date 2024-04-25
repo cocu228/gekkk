635780fb81ffec2952262f432caaa104e78372af
rezucos <tsyganok.vasya@proton.me>
2024-04-25 09:35:57 +0200

    fix - icons version

--- src/shared/ui/icons/icon-app/index.tsx
+++ src/shared/ui/icons/icon-app/index.tsx
@@ -12,6 +12,6 @@ interface IProps {
 export const IconApp:FC<IProps> = ({code,size,color,className, onClick}) => {
 
     return (
-        <svg width={size} onClick={onClick} className={`${className && className}`} fill={color} stroke={color} height={size}><use href={`/img/gek_icons_lib2.svg#${code}`}></use></svg>
+        <svg width={size} onClick={onClick} className={`${className && className}`} fill={color} stroke={color} height={size}><use href={`/img/gek_icons_lib2.svg?v2#${code}`}></use></svg>
     )
 }

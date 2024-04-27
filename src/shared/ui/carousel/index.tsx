04d14752f67d9ae3cff4038d34a6e6e1c4cd3989
rezucos <tsyganok.vasya@proton.me>
2024-04-27 07:18:37 +0200

    fix

--- src/shared/ui/carousel/index.tsx
+++ src/shared/ui/carousel/index.tsx
@@ -1,4 +1,4 @@
-import styles from "./style.module.css";
+import styles from "./styles.module.css";
 import { IconApp } from "../icons/icon-app";
 import { ReactNode, Ref, useEffect, useState } from "react";
 

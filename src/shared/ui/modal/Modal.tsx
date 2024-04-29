f8ec53b8597d72bedf6fa7c6c86189f08b2bb66e
rezucos <tsyganok.vasya@proton.me>
2024-04-30 01:25:11 +0200

    fixes

--- src/shared/ui/modal/Modal.tsx
+++ src/shared/ui/modal/Modal.tsx
@@ -1,5 +1,6 @@
 import {Modal as ModalAntd, ModalProps} from "antd";
 import styles from './styles.module.scss'
+import { IconApp } from "../icons/icon-app";
 
 type TModal = ModalProps & { padding?: boolean }
 const Modal = ({
@@ -15,7 +16,8 @@ const Modal = ({
 
     return (
         <ModalAntd open={open} destroyOnClose={destroyOnClose} footer={null} onCancel={onCancel} width={width}
-                   className={`${styles.Modal} ${className}`} {...props}>
+                   className={`${styles.Modal} ${className}`} {...props}
+                   closeIcon={<IconApp className={styles.close} code="t26" size={20} color="#7B797C"/>}>
             <div className={`${padding ? "py-10 px-8" : ""} text-gray-800 md:px-0 md:pb-0`}>
                 {children}
             </div>

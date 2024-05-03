b1f2fef5d6ece6ddd5a6c0a5e3054751c6f6bcf4
rezucos <tsyganok.vasya@proton.me>
2024-05-03 16:00:23 +0200

    removed tap selection

--- src/widgets/custom-search/index.tsx
+++ src/widgets/custom-search/index.tsx
@@ -414,14 +414,14 @@ export default function customSearch() {
               {
                 translatedOptions.map((item, ind) => (
                   <div
-                  className="w-full flex justify-between min-h-[60px] mt-2 bg-[white] rounded-lg cursor-pointer"
+                  className="w-full flex justify-between min-h-[60px] mt-2 bg-[white] text-[#29354C] active:text-[var(--gek-green)] rounded-lg cursor-pointer"
                   onClick={() => {
                     setType(item)
                     setTypeListVisibility(false)  
                   }}
                 >
                   <div className="ml-5 flex flex-row p-2 gap-5 justify-center items-center ">
-                    <span className="text-[12px] h-full flex items-center text-[#1F3446] font-bold">
+                    <span className="text-[12px] h-full flex items-center font-bold">
                       {item.label}
                     </span>
                   </div>

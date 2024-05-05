65e3b893366a8df00b01e24000381334f7dfa762
rezucos <tsyganok.vasya@proton.me>
2024-05-05 20:00:41 +0200

    Notifications API iphone fix

--- src/widgets/sidebar/ui/mobile/index.tsx
+++ src/widgets/sidebar/ui/mobile/index.tsx
@@ -46,7 +46,7 @@ const SidebarMobile = () => {
 
   return (
     <div id="sidebar" className={`${styles.Sidebar} flex flex-col`}>
-      {Notification.permission !== 'granted' ? null : <>
+      {Notification?.permission !== 'granted' ? null : <>
         <UnconfirmedTransactions/>
         <PendingTransactions/>
       </>}

92734cc503ab40ae06a6b95d446780eef9172b96
aleksmanson <aleksey.muzhichin@gmail.com>
2024-05-23 12:34:42 +0400

    gekkoin/gekkard manifests

--- vite.config.ts
+++ vite.config.ts
@@ -88,6 +88,7 @@ export default defineConfig(({mode}) => {
             }),
             svgr(),
             VitePWA({
+                //@ts-ignore
                 manifest: isGekkoin ? manifestGekkoin : manifestGekkard,
                 injectRegister: null,
             }),

f5fae2a88b1b809bfdc4dbaab958e821ccfd8050
rezucos <tsyganok.vasya@proton.me>
2024-05-24 21:00:57 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig, loadEnv} from 'vite';
 import {createHtmlPlugin} from 'vite-plugin-html';
-import {VitePWA} from 'vite-plugin-pwa'
 // import tailwindcss from 'tailwindcss'
 import react from '@vitejs/plugin-react';
 import path from "path";
@@ -89,13 +88,16 @@ export default defineConfig(({mode}) => {
                 },
             }),
             svgr(),
-            VitePWA({
-                //@ts-ignore
-                manifest: isGekwallet ?
-                    manifestGekwallet : isGekkoin ?
-                        manifestGekkoin : manifestGekkard,
-                injectRegister: null,
-            }),
+            
+            // PWA plugin
+            // VitePWA({
+            //     //@ts-ignore
+            //     manifest: isGekwallet ?
+            //         manifestGekwallet : isGekkoin ?
+            //             manifestGekkoin : manifestGekkard,
+            //     injectRegister: null,
+            // }),
+
             // VitePWA({
             //     registerType: 'autoUpdate',
             //     // includeAssets: ['**/*'],

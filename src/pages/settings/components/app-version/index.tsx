7c899baa9ba4fc9dae06d4377373cea77c60fb07
rezucos <tsyganok.vasya@proton.me>
2024-05-24 14:14:44 +0200

    build fix

--- src/pages/settings/components/app-version/index.tsx
+++ src/pages/settings/components/app-version/index.tsx
@@ -1,20 +1,14 @@
 import { MobileWrapper } from "@/shared/ui/mobile-wrapper/mobile-wrapper"
 import styles from '../../styles.module.scss'
-import { useBreakpoints } from "@/app/providers/BreakpointsProvider";
 import versions from '@/../versions.json';
 import {VersionCard} from "./version-card";
 import { useTranslation } from "react-i18next";
 
 
 export function AppVersion() {
-    const {md} = useBreakpoints();
-    
     const appVersion = import.meta.env.VITE_APP_VERSION.split("").map((elem:string) => elem.replace('"', '')).join('');
 
     const {t} = useTranslation()
-
-    console.log(versions);
-    
 
     return (
         <MobileWrapper className="w-full">
@@ -29,7 +23,7 @@ export function AppVersion() {
                 </div>
             </div>
             <div className="flex flex-col">
-                {versions.map((ver) => <VersionCard key={ver.version} date={ver.date} description={ver.description} version={ver.version}/>)}
+                {versions[import.meta.env.VITE_APP_TYPE].map((ver) => <VersionCard key={ver.version} date={ver.date} description={ver.description} version={ver.version}/>)}
             </div>
         </MobileWrapper>
     )

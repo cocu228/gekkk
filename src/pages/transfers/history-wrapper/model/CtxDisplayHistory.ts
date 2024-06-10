95c28af4d8c5c116772e9ab6f47391648807b284
rezucos <tsyganok.vasya@proton.me>
2024-06-10 10:47:55 +0200

    fix

--- src/pages/transfers/history-wrapper/model/CtxDisplayHistory.ts
+++ src/pages/transfers/history-wrapper/model/CtxDisplayHistory.ts
@@ -1,5 +1,5 @@
 import React from "react";
 
-export const CtxDisplayHistory = React.createContext<{displayHistory: () => void}>({
-    displayHistory: () => {}
+export const CtxDisplayHistory = React.createContext<{displayHistory: (value: boolean) => void}>({
+    displayHistory: (value: boolean) => {}
 });

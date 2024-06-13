71254220f082a0fba3ccae73d3b7656b0cf1a6fe
Anton <you@example.com>
2024-06-13 11:06:01 +0300

    icons auth

--- src/app/authentication/src/widgets/components/IconApp/index.tsx
+++ src/app/authentication/src/widgets/components/IconApp/index.tsx
@@ -4,18 +4,23 @@ interface IProps {
     code: string,
     className?: string,
     onClick?: () => void;
-    size: number | string,
+    size?: number | string,
     color?: string,
-    lib?: number
+    lib?: number;
+    authLib?: boolean;
+    width?: number;
+    height?: number;
 }
 
-export const IconApp: FC<IProps> = ({code, size, color, className="", onClick}) => {
+export const IconApp: FC<IProps> = ({code, size, width, height, authLib, color, className="", onClick}) => {
 
+    const widthIco = size ? size : width
+    const heightIco = size ? size : height
 
     return (
-        <svg width={size} onClick={onClick} className={`${className && className}`} fill={color} stroke={color}
-             height={size}>
-            <use href={`/img/gek_icons_lib2.svg?v#${code}`}></use>
+        <svg width={widthIco} onClick={onClick} className={`${className && className}`} fill={color} stroke={color}
+             height={heightIco}>
+            <use href={`/img/gek_icons_lib${authLib ? '3' : '2'}.svg?#${code}`}></use>
         </svg>
     )
 }

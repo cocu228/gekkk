acae49dce68a93fc987d573c8831220765da4a28
rezucos <tsyganok.vasya@proton.me>
2024-06-19 12:27:06 +0200

    test

--- src/app/chat/src/main.tsx
+++ src/app/chat/src/main.tsx
@@ -1,5 +1,5 @@
 import {render} from 'preact';
 import App from "./App";
-import "./index.css"
+import "./index.module.css";
 
 render(<App/>, document.getElementById('chat')!);
--- src/app/init.ts
+++ src/app/init.ts
@@ -10,13 +10,9 @@ document.body.setAttribute("data-app", import.meta.env.VITE_APP_TYPE);
 //Telegram integration object init
 // WebApp.ready();
 
-alert('before');
-
 if (accountId) {
     import('./index');
 } else {
     //@ts-ignore
     import('@VAR/app/authentication/{{mode-}}dist/authentication.js');
 }
-
-alert('after');

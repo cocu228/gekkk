3e11011c2377f161ef9ddc243057a142b18e6ac1
rezucos <tsyganok.vasya@proton.me>
2024-06-21 11:11:19 +0200

    wallet networks fix

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -80,7 +80,7 @@ function Wallet() {
                     <TabsGroupPrimary initValue={tab ? tab : "top_up"} callInitValue={{account, tab: tab}}>
                         <div className="grid"
                              style={{gridTemplateColumns: `repeat(${fullWidthOrHalf}, minmax(0, 1fr))`}}>
-                            <div className="substrate z-10 w-inherit relative min-h-[200px]">
+                            <div className="substrate z-10 w-inherit relative min-h-[600px]">
                                 <NetworkProvider data-tag={"top_up"} data-name={t("top_up_wallet")}>
                                     <TopUp/>
                                 </NetworkProvider>

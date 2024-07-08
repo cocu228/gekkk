935bb85569c924d73fe5b9edcd98af87c6c825b6
Anton <you@example.com>
2024-07-08 18:15:35 +0300

    new select start

---
+++ src/shared/ui/typeSelector/TypeSelector.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react'
+import s from './styles.module.scss'
+
+interface TypeSelectorProps {}
+
+export const TypeSelector:FC<TypeSelectorProps> = () => {
+    return (
+        <div className={s.TypeSelectorWrao}>
+
+        </div>
+    )
+}

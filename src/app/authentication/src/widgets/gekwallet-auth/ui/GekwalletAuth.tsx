1b29288af35a7a706dafa91946d43615c1c76f0d
dreamybo1 <idelchiksasha@gmail.com>
2024-07-11 11:12:21 +0500

    fix build

--- src/app/authentication/src/widgets/gekwallet-auth/ui/GekwalletAuth.tsx
+++ src/app/authentication/src/widgets/gekwallet-auth/ui/GekwalletAuth.tsx
@@ -1,28 +1,14 @@
 import styles from './style.module.css';
-import backgroundLogoIcon from "../../../../../../../public/img/G_logo.svg";
-import {useEffect, useState} from "preact/hooks";
-import {CallResetForm} from '../../call-reset-form/CallResetForm';
-import {ChangePasswordForm} from '../../change-password-form/ChangePasswordForm';
-import {LoginForm} from '../../login-form/LoginForm';
+import {useEffect} from "preact/hooks";
 import 'react-phone-number-input/style.css';
 import {SignIn} from "../../../shared";
 import '../../../styles/swal-material-ui.scss';
-import {useAddToHomescreenPrompt} from '../../useAddToHomescreenPrompt';
 import {CookiePolicy} from '../../cookie-policy/CookiePolicy';
 // import SupportChatUnauthorized from '../chat';
-import { IconApp } from "../../components/IconApp"
 import { Header } from '../../header/header/Header';
-import { IForm } from '../types';
 
 export const GekwalletAuth = () => {
-    const [detailsActive, setDetailsActive] = useState(false)
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const emailCode = urlParams.get('emailCode');
-    const [phone, setPhone] = useState<string>('');
 
-    const [promptable, promptToInstall, isInstalled] = useAddToHomescreenPrompt();
-    const [form, setForm] = useState<IForm>(!emailCode ? 'LOGIN' : 'RESET_PASSWORD');
 
     // const [chatOpened, setChatOpened] = useState<boolean>(false)
 

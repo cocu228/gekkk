60059219fcf9bd95db22aa310de3d761278292a1
dreamybo1 <idelchiksasha@gmail.com>
2024-07-11 15:33:21 +0500

    gekwallet auth

--- src/app/authentication/src/widgets/gekwallet-auth/ui/GekwalletAuth.tsx
+++ src/app/authentication/src/widgets/gekwallet-auth/ui/GekwalletAuth.tsx
@@ -6,12 +6,13 @@ import '../../../styles/swal-material-ui.scss';
 import {CookiePolicy} from '../../cookie-policy/CookiePolicy';
 // import SupportChatUnauthorized from '../chat';
 import { Header } from '../../header/header/Header';
-
+import gekLogo from '../../../images/gek-logo.svg'
+import { IconApp } from '../../components/IconApp';
+import useResponsiveWidth from '../../../shared/hooks/useResponsiveWidth';
 export const GekwalletAuth = () => {
 
-
-    // const [chatOpened, setChatOpened] = useState<boolean>(false)
-
+    const {width} = useResponsiveWidth()
+    
     useEffect(() => {
         const runCMA = async () => {
             // Availability of `window.PublicKeyCredential` means WebAuthn is usable.
@@ -38,13 +39,29 @@ export const GekwalletAuth = () => {
         <>
             <Header code='w10' />
             <div className={styles.Main}>
-                <div className={styles.Outline}>
-                    <div className={styles.Dashed}>
-
+                <div className={styles.Wrapper}>
+                    <div className={styles.Outline}>
+                        <div className={styles.Dashed}>
+                            <div className={styles.ImageWrapper}>
+                                <img className={styles.GekLogo} src={gekLogo} alt="" />
+                            </div>
+                            <div className={styles.DeviceKeyWrapper}>
+                                <button className={styles.DeviceKey}>
+                                    + Add new device key
+                                </button>
+                            </div>
+                        </div>
+                        <button title='Log in' className={styles.Login}>
+                            <div className={styles.Round}/>
+                            <span className={styles.LoginTitle}>Log in</span>
+                        </button>
                     </div>
-                    <div className={styles.Login}>
-                        <div className={styles.Round}/>
-                        <span className={styles.LoginTitle}>Log in</span>
+                    <div className={styles.Footer}>
+                        {width > 768 && <button title="Registration" className={styles.Registration}>Registration</button>}
+                        <button className={styles.Install}>
+                            <IconApp code='w6' size={20} />
+                            <span title="Install Gekwallet">Install Gekwallet</span>
+                        </button>
                     </div>
                 </div>
             </div>

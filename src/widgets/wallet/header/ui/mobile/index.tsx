43aaf0b8734be1158a783bdc6414d119e5ffdf81
rezucos <tsyganok.vasya@proton.me>
2024-07-22 13:14:40 +0200

    Fix

--- src/widgets/wallet/header/ui/mobile/index.tsx
+++ src/widgets/wallet/header/ui/mobile/index.tsx
@@ -132,13 +132,13 @@ const WalletHeaderMobile = () => {
         <div className={styles.EurGekkoinPrice}>
           {isEUR && !!balance?.bank_account_status && (
             <div className='flex group gap-[4px] items-center'>
-              <span className='text-[#B9B9B5] text-[12px] font-bold flex items-center gap-[5px]'>{t("account_status").capitalize()}:</span>
-              <p
-                className={`text-[12px] font-normal ${
-                  balance.bank_account_status === "ACTIVE" ? "text-[#B9B9B5]" : "text-[#8F123A]"
-                }`}
-              >
-                {balance.bank_account_status}
+              <span className='text-[#B9B9B5] text-[12px] font-medium flex items-center gap-[5px]'>
+                {t('account_status').capitalize()}:
+              </span>
+              <p className={`${balance.bank_account_status === 'ACTIVE'
+                ? 'text-[var(--gek-mid-grey)]'
+                : 'text-[var(--gek-red)]'} text-[12px] font-medium`}>
+                {t(`bank_status.${balance.bank_account_status}`).capitalize()}
               </p>
             </div>
           )}

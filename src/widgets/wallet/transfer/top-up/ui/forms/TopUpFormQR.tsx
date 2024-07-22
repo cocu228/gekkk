e244cb636c737febce25dceba6dd0d4b3968185f
Anton <you@example.com>
2024-07-22 09:38:23 +0300

    top up fix

--- src/widgets/wallet/transfer/top-up/ui/forms/TopUpFormQR.tsx
+++ src/widgets/wallet/transfer/top-up/ui/forms/TopUpFormQR.tsx
@@ -7,15 +7,20 @@ import Button from "@/shared/ui/button/Button";
 import { CtxWalletNetworks, CtxWalletData } from "@/widgets/wallet/transfer/model/context";
 import useError from "@/shared/model/hooks/useError";
 import { IconApp } from "@/shared/ui/icons/icon-app";
+import ReactQRCode from "react-qr-code";
+import ClipboardField from "@/shared/ui/clipboard-field/ClipboardField";
 
 import { getChosenNetwork } from "../../../model/helpers";
 import styles from "./style.module.scss";
+import { useBreakpoints } from "@/app/providers/BreakpointsProvider";
+import CopyIcon from "@/shared/ui/copy-icon/CopyIcon";
 
 const TopUpFormQR = () => {
-  const { $const } = useContext(CtxWalletData);
+  const { $const, name } = useContext(CtxWalletData);
   const [localErrorHunter, , localErrorInfoBox] = useError();
   const { t } = useTranslation();
-  const { setRefresh, setLoading, tokenNetworks, networkTypeSelect } = useContext(CtxWalletNetworks);
+  const { setRefresh, setLoading, tokenNetworks, addressesForQR, networkTypeSelect } = useContext(CtxWalletNetworks);
+  const { md } = useBreakpoints();
 
   const onCreateAddress = async () => {
     setLoading(true);
@@ -30,7 +35,43 @@ const TopUpFormQR = () => {
   };
 
   return (
-    <>
+    addressesForQR !== null &&
+    (addressesForQR !== undefined ? (
+      <>
+        <div className={styles.QRContainer}>
+          <h3 className={styles.QRSendText}>
+            {t("send_to_this")}{" "}
+            <b>
+              {$const} {name}
+            </b>{" "}
+            {t("address_small")}
+          </h3>
+
+          <div className={styles.QRWrapper}>
+            <div className={styles.QRWrapperSecond}>
+              <ReactQRCode className={styles.QRWrapperThird} value={addressesForQR} viewBox={`0 0 148 148`} />
+            </div>
+          </div>
+          {!md ? (
+            <div className={styles.QRFieldDesktop}>
+              <ClipboardField value={addressesForQR} />
+            </div>
+          ) : (
+            <div className={styles.QRFieldMobile}>
+              <div className='col'>
+                <div className={styles.QRFieldMobileWrapper}>
+                  <div className={styles.QRFieldMobileWrapperSecond}>
+                    <span className={styles.QRFieldMobileValue}>{addressesForQR}</span>
+                    <CopyIcon value={addressesForQR} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </>
+    ) : (
+      <>
       <div className={styles.GenerateQR}>
         <div className={styles.GenerateQRAttention}>
           <div className={styles.GenerateQRAttentionIcon}>
@@ -48,61 +89,7 @@ const TopUpFormQR = () => {
       </div>
       <div className={styles.GenerateQRAttentionError}>{localErrorInfoBox}</div>
     </>
-    // addressesForQR !== null &&
-    // (addressesForQR !== undefined ? (
-    //   <>
-    //     <div className={styles.QRContainer}>
-    //       <h3 className={styles.QRSendText}>
-    //         {t("send_to_this")}{" "}
-    //         <b>
-    //           {$const} {name}
-    //         </b>{" "}
-    //         {t("address_small")}
-    //       </h3>
-
-    //       <div className={styles.QRWrapper}>
-    //         <div className={styles.QRWrapperSecond}>
-    //           <ReactQRCode className={styles.QRWrapperThird} value={addressesForQR} viewBox={`0 0 148 148`} />
-    //         </div>
-    //       </div>
-    //       {!md ? (
-    //         <div className={styles.QRFieldDesktop}>
-    //           <ClipboardField value={addressesForQR} />
-    //         </div>
-    //       ) : (
-    //         <div className={styles.QRFieldMobile}>
-    //           <div className='col'>
-    //             <div className={styles.QRFieldMobileWrapper}>
-    //               <div className={styles.QRFieldMobileWrapperSecond}>
-    //                 <span className={styles.QRFieldMobileValue}>{addressesForQR}</span>
-    //                 <CopyIcon value={addressesForQR} />
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       )}
-    //     </div>
-    //   </>
-    // ) : (
-    //   <>
-    //     <div className={styles.GenerateQR}>
-    //       <div className={styles.GenerateQRAttention}>
-    //         <div className={styles.GenerateQRAttentionIcon}>
-    //           <IconApp code='t27' size={15} color='var(--gek-red)' />
-    //         </div>
-    //         <span className={styles.GenerateQRAttentionText}>
-    //           {t("you_should_send_only")} <b>{$const}</b> {t("you_should_send_only_2")}
-    //         </span>
-    //       </div>
-    //       <div className='flex justify-center w-full'>
-    //         <Button size='lg' color='blue' htmlType='submit' className='w-full' onClick={onCreateAddress}>
-    //           {t("generate_address")}
-    //         </Button>
-    //       </div>
-    //     </div>
-    //     <div className={styles.GenerateQRAttentionError}>{localErrorInfoBox}</div>
-    //   </>
-    // ))
+    ))
   );
 };
 
